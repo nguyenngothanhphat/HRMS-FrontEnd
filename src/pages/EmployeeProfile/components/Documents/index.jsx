a7d87ba00adb98c1daff78543538696c90d8236a
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-09-10 14:06:13 +0700

    almost done DocumentTab

--- src/pages/EmployeeProfile/components/Documents/index.jsx
+++ src/pages/EmployeeProfile/components/Documents/index.jsx
@@ -2,29 +2,90 @@ import { Layout, Row, Col } from 'antd';
 import React, { PureComponent } from 'react';
 import InfoCollapseType2 from './InfoCollapseType2';
 
+const data = [
+  {
+    title: 'Offer Letter',
+    files: [
+      {
+        fileName: 'Abc.txt',
+        generatedBy: 'Terralogic',
+        date: '20/12/2020'
+      },
+      {
+        fileName: 'Cdf.txt',
+        generatedBy: 'Terralogic',
+        date: '20/12/2020'
+      }
+    ]
+  },
+  {
+    title: 'Tax Documents',
+    files: [
+      {
+        fileName: 'aaaaaaaaaa.txt',
+        generatedBy: 'Terralogic',
+        date: '20/12/2020'
+      }
+    ]
+  },
+  {
+    title: 'Tax Documents',
+    files: [
+      {
+        fileName: 'aaaaaaaaaa.txt',
+        generatedBy: 'Terralogic',
+        date: '20/12/2020'
+      }
+    ]
+  }
+]
+
+const data1 = [
+  {
+    title: 'Offer Letter',
+    files: [
+      {
+        fileName: 'Abc.txt',
+        generatedBy: 'Terralogic',
+        date: '20/12/2020'
+      },
+      {
+        fileName: 'Cdf.txt',
+        generatedBy: 'Terralogic',
+        date: '20/12/2020'
+      },
+      {
+        fileName: 'Cdf.txt',
+        generatedBy: 'Terralogic',
+        date: '20/12/2020'
+      }
+    ]
+  },
+  {
+    title: 'Tax Documents',
+    files: [
+      {
+        fileName: 'aaaaaaaaaa.txt',
+        generatedBy: 'Terralogic',
+        date: '20/12/2020'
+      }
+    ]
+  }
+]
+
 class Documents extends PureComponent {
     render () {
         return (
           <Layout>
             <Row>
-              <Col span={12}>
-                <InfoCollapseType2 />
+              <Col xs={24} lg={12}>
+                <InfoCollapseType2 data={data} />
+                <InfoCollapseType2 data={data1} />
+                <InfoCollapseType2 data={data} />
               </Col>
-              <Col span={12}>
-                <InfoCollapseType2 />
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <InfoCollapseType2 />
-              </Col>
-              <Col span={12}>
-                <InfoCollapseType2 />
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <InfoCollapseType2 />
+              <Col xs={24} lg={12}>
+                <InfoCollapseType2 data={data1} />
+                <InfoCollapseType2 data={data} />
               </Col>
             </Row>
           </Layout>
