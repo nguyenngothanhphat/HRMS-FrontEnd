9afdd769ccac3cb22e041f146f1a71bad0c1d948
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-09-11 08:46:40 +0700

    done UI Document Tab

--- src/pages/EmployeeProfile/components/Documents/index.jsx
+++ src/pages/EmployeeProfile/components/Documents/index.jsx
@@ -1,6 +1,7 @@
 import { Layout, Row, Col } from 'antd';
 import React, { PureComponent } from 'react';
 import InfoCollapseType2 from './InfoCollapseType2';
+import styles from './index.less';
 
 const data = [
   {
@@ -153,14 +154,14 @@ const data = [
 class Documents extends PureComponent {
   render() {
     return (
-      <Layout>
-        <Row>
-          <Col xs={24} lg={12}>
+      <Layout className={styles.Documents}>
+        <Row className={styles.documentRow}>
+          <Col xs={24} lg={12} className={styles.documentCol}>
             {data.map((value, index) =>
               index % 2 === 0 ? <InfoCollapseType2 data={value} /> : '',
             )}
           </Col>
-          <Col xs={24} lg={12}>
+          <Col xs={24} lg={12} className={styles.documentCol}>
             {data.map((value, index) =>
               index % 2 !== 0 ? <InfoCollapseType2 data={value} /> : '',
             )}
