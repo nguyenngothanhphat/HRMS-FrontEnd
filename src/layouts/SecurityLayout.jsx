10d610a35d05758fb9f1d590cb7e84d0b147632f
phat nguyen <phat.nguyen@terralogic.com>
2020-09-14 17:42:22 +0700

    add API get current user.

--- src/layouts/SecurityLayout.jsx
+++ src/layouts/SecurityLayout.jsx
@@ -10,11 +10,12 @@ class SecurityLayout extends React.Component {
   };
 
   componentDidMount() {
+    const token = getToken();
     this.setState({
       isReady: true,
     });
     const { dispatch } = this.props;
-    if (dispatch) {
+    if (token) {
       dispatch({
         type: 'user/fetchCurrent',
       });
@@ -26,7 +27,7 @@ class SecurityLayout extends React.Component {
     const { children, loading, currentUser } = this.props; // You can replace it to your authentication rule (such as check token exists)
     // 你可以把它替换成你自己的登录认证规则（比如判断 token 是否存在）
     const token = getToken();
-    const isLogin = currentUser && currentUser.userid;
+    const isLogin = currentUser && currentUser._id;
     const queryString = stringify({
       redirect: window.location.href,
     });
