90e0358215f3c071576160f46ae202a2b836c002
quan-illogical <quan.nguyen@terralogic.com>
2020-09-16 10:24:08 +0700

    finished third step

---
+++ src/pages/EmployeeProfile/components/EmploymentTab/components/HandleChanges/components/ThirdStep/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Select, Input } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import styles from './styles.less';
+
+export default function ThirdStep(props) {
+  const { Option } = Select;
+  const { onChange, onSearch } = props;
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <div className={styles.headings}>What do you wish to change?</div>
+      <div className={styles.select}>
+        <div>Department</div>
+        <Select
+          suffixIcon={<DownOutlined style={{ color: 'black' }} />}
+          showSearch
+          placeholder="Select a department"
+          optionFilterProp="children"
+          onChange={(value) => onChange(value, 'department')}
+          onSearch={onSearch}
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+        >
+          {['Engineer', 'HR', 'Support', 'Design'].map((item) => {
+            return <Option value={item}>{item}</Option>;
+          })}
+          ]
+        </Select>
+      </div>
+      <div className={styles.select}>
+        <div>Position</div>
+        <Input
+          style={{ width: 300 }}
+          placeholder="Enter the position"
+          onChange={(e) => onChange(e.target.value, 'position')}
+        />
+      </div>
+      <div className={styles.select}>
+        <div>Reporting to</div>
+        <Select
+          suffixIcon={<DownOutlined style={{ color: 'black' }} />}
+          showSearch
+          placeholder=""
+          optionFilterProp="children"
+          onChange={(value) => onChange(value, 'reportTo')}
+          onSearch={onSearch}
+          filterOption={(input, option) =>
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          }
+        >
+          {['Anil Reddy'].map((item) => {
+            return <Option value={item}>{item}</Option>;
+          })}
+          ]
+        </Select>
+      </div>
+    </div>
+  );
+}
--- src/pages/EmployeeProfile/components/EmploymentTab/components/HandleChanges/index.jsx
+++ src/pages/EmployeeProfile/components/EmploymentTab/components/HandleChanges/index.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styles from './styles.less';
 import FirstStep from './components/FirstStep';
 import SecondStep from './components/SecondStep';
+import ThirdStep from './components/ThirdStep';
 
 export default function HandleChanges(props) {
   const { current } = props;
@@ -21,6 +22,7 @@ export default function HandleChanges(props) {
     <div className={styles.handleChanges}>
       {current === 0 ? <FirstStep onRadioChange={onRadioChange} radio={radio} /> : null}
       {current === 1 ? <SecondStep onChange={onChange} onSearch={onSearch} /> : null}
+      {current === 2 ? <ThirdStep onChange={onChange} onSearch={onSearch} /> : null}
     </div>
   );
 }
