2af2746a8246e7752e1d810dd97f512e109dd3ca
phat nguyen <phat.nguyen@terralogic.com>
2020-09-16 12:45:46 +0700

    improve UI

--- src/pages/EmployeeProfile/components/GeneralInfo/components/ProfessionalAcademicBackground/components/View/index.jsx
+++ src/pages/EmployeeProfile/components/GeneralInfo/components/ProfessionalAcademicBackground/components/View/index.jsx
@@ -11,6 +11,17 @@ export default class View extends PureComponent {
       { label: 'Total Experience', value: '12 Years' },
       { label: 'Qualification', value: '12th PUC' },
     ];
+
+    const listCertification = [
+      {
+        name: 'HCI Certification',
+        urlFile: 'https://www.mariekuter.com/wp-content/uploads/2014/09/hci-coursera-statement.jpg',
+      },
+      {
+        name: 'UX Academy Certification',
+        urlFile: 'https://media.nngroup.com/media/editor/2019/02/13/ux-certificate-example.png',
+      },
+    ];
     return (
       <Row gutter={[0, 16]} className={styles.root}>
         {dummyData.map((item) => (
@@ -23,7 +34,35 @@ export default class View extends PureComponent {
             </Col>
           </Fragment>
         ))}
-        {/* Custom Col Here */}
+        <Col span={6} className={styles.textLabel}>
+          Certifications
+        </Col>
+        <Col span={18}>
+          {listCertification.map((item, index) => {
+            const { name = '', urlFile = '' } = item;
+            return (
+              <div className={styles.viewRow} style={{ marginBottom: '6px' }}>
+                <div className={styles.textValue}>{`${index + 1} - ${name}`}</div>
+                <div className={styles.viewRow}>
+                  <a
+                    href={urlFile}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    id="img-certification"
+                    className={styles.nameCertification}
+                  >
+                    {name}
+                  </a>
+                  <img
+                    src="/assets/images/iconFilePNG.svg"
+                    alt="iconFilePNG"
+                    className={styles.iconCertification}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </Col>
       </Row>
     );
   }
