34ca93e73c393436f1496c9a52e9ad3ec2a38f13
phat nguyen <phat.nguyen@terralogic.com>
2020-09-16 11:16:50 +0700

    improve UI

--- src/pages/EmployeeProfile/components/GeneralInfo/components/ProfessionalAcademicBackground/index.jsx
+++ src/pages/EmployeeProfile/components/GeneralInfo/components/ProfessionalAcademicBackground/index.jsx
@@ -1,10 +1,17 @@
-import React, { PureComponent } from 'react';
-// import { Row, Col } from 'antd';
+import React, { PureComponent, Fragment } from 'react';
+import { Row, Col } from 'antd';
 import { EditFilled } from '@ant-design/icons';
 import styles from './index.less';
 
 class ProfessionalAcademicBackground extends PureComponent {
   render() {
+    const dummyData = [
+      { label: 'Previous Job label', value: 'Senior UX Designer' },
+      { label: 'Previous Company', value: 'Apple' },
+      { label: 'Past Experience', value: '5 Years' },
+      { label: 'Total Experience', value: '12 Years' },
+      { label: 'Qualification', value: '12th PUC' },
+    ];
     return (
       <div className={styles.root}>
         <div className={styles.viewTitle}>
@@ -14,7 +21,21 @@ class ProfessionalAcademicBackground extends PureComponent {
             <p className={styles.viewTitle__edit__text}>Edit</p>
           </div>
         </div>
-        <div className={styles.viewBottom}>bottom</div>
+        <div className={styles.viewBottom}>
+          <Row gutter={[0, 16]}>
+            {dummyData.map((item) => (
+              <Fragment key={item.label}>
+                <Col span={6} className={styles.textLabel}>
+                  {item.label}
+                </Col>
+                <Col span={18} className={styles.textValue}>
+                  {item.value}
+                </Col>
+              </Fragment>
+            ))}
+            {/* Custom Col Here */}
+          </Row>
+        </div>
       </div>
     );
   }
