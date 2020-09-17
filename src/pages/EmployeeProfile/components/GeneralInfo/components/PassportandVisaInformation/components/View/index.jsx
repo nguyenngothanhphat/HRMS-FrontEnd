a384700ad1923794d1804c951c83aa3fcdcd7fce
INI\anh.nguyen <anh.nguyen@terralogic.com>
2020-09-17 17:00:34 +0700

    get data and edit with Redux

--- src/pages/EmployeeProfile/components/GeneralInfo/components/PassportandVisaInformation/components/View/index.jsx
+++ src/pages/EmployeeProfile/components/GeneralInfo/components/PassportandVisaInformation/components/View/index.jsx
@@ -1,14 +1,44 @@
 import React, { PureComponent, Fragment } from 'react';
 import { Row, Col } from 'antd';
+import Moment from 'moment';
 import styles from './index.less';
 
 class View extends PureComponent {
   render() {
+    const { dataAPI } = this.props;
     const dummyData = [
-      { label: 'Passport Number', value: 'PNHG0000993748' },
-      { label: 'Issued Country', value: 'India' },
-      { label: 'Issued On', value: '22-05-2025' },
-      { label: 'Valid Till', value: '22-05-2025' },
+      { label: 'Passport Number', value: dataAPI.passportNo },
+      { label: 'Issued Country', value: dataAPI.passportIssueCountry },
+      {
+        label: 'Issued On',
+        value: dataAPI.issuedOnPassPort
+          ? Moment(dataAPI.issuedOnPassPort).locale('en').format('Do MMM YYYY')
+          : '',
+      },
+      {
+        label: 'Valid Till',
+        value: dataAPI.validTillPassPort
+          ? Moment(dataAPI.validTillPassPort).locale('en').format('Do MMM YYYY')
+          : '',
+      },
+    ];
+    const dummyData2 = [
+      { label: 'Visa Number', value: dataAPI.visaNumber },
+      { label: 'Visa Type', value: dataAPI.visaType },
+      { label: 'Country', value: dataAPI.country },
+      { label: 'Entry Type', value: dataAPI.entryType },
+      {
+        label: 'Issued On',
+        value: dataAPI.issuedOnVisa
+          ? Moment(dataAPI.issuedOnVisa).locale('en').format('Do MMM YYYY')
+          : '',
+      },
+      {
+        label: 'Valid Till',
+        value: dataAPI.validTillVisa
+          ? Moment(dataAPI.validTillVisa).locale('en').format('Do MMM YYYY')
+          : '',
+      },
     ];
     return (
       <Row gutter={[0, 16]} className={styles.root}>
@@ -22,6 +52,18 @@ class View extends PureComponent {
             </Col>
           </Fragment>
         ))}
+        <Col span={24} className={styles.line} />
+        {dummyData2.map((item) => (
+          <Fragment key={item.label}>
+            <Col span={6} className={styles.textLabel}>
+              {item.label}
+            </Col>
+            <Col span={18} className={styles.textValue}>
+              {item.value}
+            </Col>
+          </Fragment>
+        ))}
+
         {/* Custom Col Here */}
       </Row>
     );
