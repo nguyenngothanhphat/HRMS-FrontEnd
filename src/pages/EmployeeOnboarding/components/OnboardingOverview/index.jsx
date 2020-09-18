85a8706bae959dad965e88d7070e8aa1e97fb1d8
INI\liam.nguyen <liam.nguyen@terralogic.com>
2020-09-18 14:59:42 +0700

    Add tabs

---
+++ src/pages/EmployeeOnboarding/components/OnboardingOverview/index.jsx
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react';
+import { Button } from 'antd';
+import { Link } from 'umi';
+
+class OnboardingOverview extends PureComponent {
+  render() {
+    return (
+      <div>
+        <p>OnboardingOverview</p>
+        <Link to="/employee-onboarding/add">
+          <Button type="primary">Add Team Member</Button>
+        </Link>
+        <Link
+          to="/employee-onboarding/review/16003134"
+          style={{ marginTop: '1rem', display: 'block' }}
+        >
+          Link review member by rookieId =16003134
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default OnboardingOverview;
--- src/pages/EmployeeOnboarding/index.jsx
+++ src/pages/EmployeeOnboarding/index.jsx
@@ -1,23 +1,32 @@
 import React, { PureComponent } from 'react';
 import { PageContainer } from '@/layouts/layout/src';
-import { Button } from 'antd';
-import { Link } from 'umi';
+import { Tabs } from 'antd';
+
+import OnboardingOverview from './components/OnboardingOverview';
+import Settings from './components/Settings';
+import CustomFields from './components/CustomFields';
+
 import styles from './index.less';
 
 export default class EmployeeOnboarding extends PureComponent {
   render() {
+    const { TabPane } = Tabs;
     return (
       <PageContainer>
         <div className={styles.containerEmployeeOnboarding}>
-          <Link to="/employee-onboarding/add">
-            <Button type="primary">Add Team Member</Button>
-          </Link>
-          <Link
-            to="/employee-onboarding/review/16003134"
-            style={{ marginTop: '1rem', display: 'block' }}
-          >
-            Link review member by rookieId =16003134
-          </Link>
+          <div className={styles.employeeOnboarding__tab}>
+            <Tabs defaultActiveKey="1">
+              <TabPane tab="Onboarding overview" key="1">
+                <OnboardingOverview />
+              </TabPane>
+              <TabPane tab="Settings" key="2">
+                <Settings />
+              </TabPane>
+              <TabPane tab="Custom field" key="3">
+                <CustomFields />
+              </TabPane>
+            </Tabs>
+          </div>
         </div>
       </PageContainer>
     );
