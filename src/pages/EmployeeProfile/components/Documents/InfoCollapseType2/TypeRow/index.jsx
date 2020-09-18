d6cd421afd28de43392e5afcf1f2f7527c76f62d
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-09-18 11:24:24 +0700

    fix arrow of each panel

--- src/pages/EmployeeProfile/components/Documents/InfoCollapseType2/TypeRow/index.jsx
+++ src/pages/EmployeeProfile/components/Documents/InfoCollapseType2/TypeRow/index.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState, PureComponent } from 'react';
 import { Collapse, Row, Col, Menu, Dropdown } from 'antd';
 import { EllipsisOutlined } from '@ant-design/icons';
 import FileIcon from '../../../../../../assets/pdf_icon.png';
@@ -8,27 +8,24 @@ import UpArrowIcon from '../../../../../../assets/upArrow.svg';
 import styles from './index.less';
 
 const { Panel } = Collapse;
-class TypeRow extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: true,
-    };
-  }
 
-  preventCollapse = (event) => {
+const CollapseTest = (props) => {
+  const { data, onFileClick } = props;
+  const [row] = useState(data);
+  const [open, setOpen] = useState(true);
+  const preventCollapse = (event) => {
     event.stopPropagation();
   };
 
-  handleDownloadClick = (event) => {
-    this.preventCollapse(event);
+  const handleDownloadClick = (event) => {
+    preventCollapse(event);
   };
 
-  handleMenuClick = (event) => {
-    this.preventCollapse(event);
+  const handleMenuClick = (event) => {
+    preventCollapse(event);
   };
 
-  menu = () => (
+  const menu = () => (
     <Menu>
       <Menu.Item>
         <div>1st menu item</div>
@@ -40,25 +37,25 @@ class TypeRow extends PureComponent {
     </Menu>
   );
 
-  statusAndButtons = () => (
+  const statusAndButtons = () => (
     <div className={styles.statusAndButtons}>
       <a>Complete</a>
       <img
         alt="download"
         src={DownloadIcon}
         className={styles.downloadButton}
-        onClick={this.handleDownloadClick}
+        onClick={handleDownloadClick}
       />
-      <Dropdown overlay={this.menu}>
-        <EllipsisOutlined onClick={this.handleMenuClick} className={styles.menuButton} />
+      <Dropdown overlay={menu}>
+        <EllipsisOutlined onClick={handleMenuClick} className={styles.menuButton} />
       </Dropdown>
     </div>
   );
 
-  headerWithArrowIcon = (open, title) => (
+  const headerWithArrowIcon = (openTest, title) => (
     <div className={styles.headerWithArrowIcon}>
       <span>{title}</span>
-      {open ? (
+      {openTest ? (
         <img src={UpArrowIcon} alt="up-arrow" />
       ) : (
         <img src={DownArrowIcon} alt="down-arrow" />
@@ -66,55 +63,48 @@ class TypeRow extends PureComponent {
     </div>
   );
 
-  // customArrowIcon = (isActive) =>
-  //   isActive ? (
-  //     <img src={UpArrowIcon} alt="up-arrow" />
-  //   ) : (
-  //     <img src={DownArrowIcon} alt="down-arrow" />
-  //   );
-
-  onChange = () => {
-    this.setState((prevState) => ({
-      open: !prevState.open,
-    }));
+  const onChange = () => {
+    setOpen(!open);
   };
 
+  return (
+    <Collapse
+      defaultActiveKey={['1']}
+      onChange={onChange}
+      bordered={false}
+      className={styles.eachCollapse}
+    >
+      <Panel
+        className={styles.eachPanel}
+        key="1"
+        showArrow={false}
+        header={headerWithArrowIcon(open, row.kind)}
+        extra={statusAndButtons()}
+      >
+        {row.files.map((file) => (
+          <Row id={file.id} className={styles.eachRow}>
+            <Col span={8} className={styles.fileName}>
+              <div onClick={() => onFileClick(file.id)}>
+                <img src={FileIcon} alt="file" className={styles.fileIcon} />
+                <span>{file.fileName}</span>
+              </div>
+            </Col>
+            <Col span={7}>{file.generatedBy}</Col>
+            <Col span={7}>{file.date}</Col>
+            <Col span={2} />
+          </Row>
+        ))}
+      </Panel>
+    </Collapse>
+  );
+};
+class TypeRow extends PureComponent {
   render() {
     const { data = [], onFileClick } = this.props;
-    const { open } = this.state;
     return (
       <div>
         {data.map((row) => (
-          <Collapse
-            defaultActiveKey={['1']}
-            onChange={this.onChange}
-            bordered={false}
-            // expandIcon={({ isActive }) => this.customArrowIcon(isActive)}
-            // expandIconPosition="right"
-            className={styles.eachCollapse}
-          >
-            <Panel
-              className={styles.eachPanel}
-              key="1"
-              showArrow={false}
-              header={this.headerWithArrowIcon(open, row.kind)}
-              extra={this.statusAndButtons()}
-            >
-              {row.files.map((file) => (
-                <Row id={file.id} className={styles.eachRow}>
-                  <Col span={8} className={styles.fileName}>
-                    <div onClick={() => onFileClick(file.id)}>
-                      <img src={FileIcon} alt="file" className={styles.fileIcon} />
-                      <span>{file.fileName}</span>
-                    </div>
-                  </Col>
-                  <Col span={7}>{file.generatedBy}</Col>
-                  <Col span={7}>{file.date}</Col>
-                  <Col span={2} />
-                </Row>
-              ))}
-            </Panel>
-          </Collapse>
+          <CollapseTest onFileClick={onFileClick} data={row} />
         ))}
       </div>
     );
