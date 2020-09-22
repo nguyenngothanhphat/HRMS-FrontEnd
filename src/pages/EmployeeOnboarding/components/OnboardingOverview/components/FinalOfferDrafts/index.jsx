5be15325572986ef48338c315a69e1ada823be75
Duy Tran <txd22081999@gmail.com>
2020-09-22 17:19:47 +0700

    Add table

--- src/pages/EmployeeOnboarding/components/OnboardingOverview/components/FinalOfferDrafts/index.jsx
+++ src/pages/EmployeeOnboarding/components/OnboardingOverview/components/FinalOfferDrafts/index.jsx
@@ -1,9 +1,33 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+import { Table } from 'antd';
+import { EllipsisOutlined } from '@ant-design/icons';
+import { formatMessage } from 'umi';
+
+import {
+  rookieList,
+  COLUMN_NAME,
+  TABLE_TYPE,
+} from '@/pages/EmployeeOnboarding/components/OnboardingOverview/components/utils';
+import OnboardTable from '@/pages/EmployeeOnboarding/components/OnboardingOverview/components/OnboardTable';
 import styles from './index.less';
 
-class FinalOfferDrafts extends PureComponent {
+const list = rookieList.filter((rookie) => rookie.isNew != true);
+const { ID, NAME, POSITION, LOCATION, DATE_JOIN, ACTION } = COLUMN_NAME;
+const { FINAL_OFFERS_DRAFTS } = TABLE_TYPE;
+
+class FinalOfferDrafts extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <div>FinalOfferDrafts</div>;
+    return (
+      <OnboardTable
+        list={rookieList}
+        columnArr={[ID, NAME, POSITION, LOCATION, DATE_JOIN, ACTION]}
+        type={FINAL_OFFERS_DRAFTS}
+      />
+    );
   }
 }
 
