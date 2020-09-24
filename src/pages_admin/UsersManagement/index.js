df216f33d10f5ce2b20cca11f7fc8b61cc6d4d9f
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-09-24 10:33:59 +0700

    fix bugs

--- src/pages_admin/UsersManagement/index.js
+++ src/pages_admin/UsersManagement/index.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react';
 import { PageContainer } from '@/layouts/layout/src';
 import { formatMessage } from 'umi';
-import { Tabs } from 'antd';
 import styles from './index.less';
 import TableContainer from './components/TableContainer';
 
@@ -11,15 +10,13 @@ export default class UsersManagement extends PureComponent {
   };
 
   render() {
-    const { TabPane } = Tabs;
     return (
       <PageContainer>
         <div className={styles.containerUsers}>
-          <Tabs defaultActiveKey="1" tabBarExtraContent={this.operations()}>
-            <TabPane tab={formatMessage({ id: 'pages_admin.users.title' })} key="1">
-              <TableContainer />
-            </TabPane>
-          </Tabs>
+          <div className={styles.headerText}>
+            <span>{formatMessage({ id: 'pages_admin.users.title' })}</span>
+          </div>
+          <TableContainer />
         </div>
       </PageContainer>
     );
