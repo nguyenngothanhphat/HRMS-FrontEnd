73dbfe6b19f4222196e3845cdb46828c37236032
Phuong-Van <phuong.van@terralogic.com>
2020-09-25 07:01:50 +0700

    Download career graph

--- src/pages/EmployeeProfile/components/PerformanceHistory/components/CareerGraph/index.jsx
+++ src/pages/EmployeeProfile/components/PerformanceHistory/components/CareerGraph/index.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
-import { Card, Row, Button } from 'antd';
+import { Card, Button } from 'antd';
 import Chart from 'react-google-charts';
+import { jsPDF as JsPDF } from 'jspdf';
 import styles from './index.less';
 
 class CareerGraph extends PureComponent {
@@ -36,119 +37,128 @@ class CareerGraph extends PureComponent {
   render() {
     const dummyData = {
       averageScore: 7.7,
-      chartDate: [
+      chartData: [
         {
           year: 2018,
           value: 4,
+          performanceDetail: {
+            salaryHike: '20%',
+            promotedTo: 'PM',
+            roleChangedTo: 'Manager',
+          },
         },
         {
           year: 2019,
           value: 5,
+          performanceDetail: {
+            salaryHike: '10%',
+            promotedTo: 'PM',
+            roleChangedTo: 'Senior',
+          },
         },
         {
           year: 2020,
           value: 7,
+          performanceDetail: {
+            salaryHike: '15%',
+            promotedTo: 'PM',
+            roleChangedTo: 'Software Engineer 1',
+          },
         },
         {
           year: 2021,
           value: 8,
+          performanceDetail: {
+            salaryHike: '5%',
+            promotedTo: 'PM',
+            roleChangedTo: 'Manager',
+          },
         },
       ],
+    };
+
+    const chartEvents = {
+      eventName: 'ready',
+      callback: ({ chartWrapper }) => {
+        const chart = chartWrapper.getChart();
+        const btnSave = document.getElementById('btn_download');
+        btnSave.addEventListener(
+          'click',
+          () => {
+            const doc = new JsPDF();
+            doc.addImage(chart.getImageURI(), 0, 0);
+            doc.save('ChartCareerGraph.pdf');
+          },
+          false,
+        );
+      },
+    };
+
+    const options = {
+      hAxis: {
+        title: 'Duration',
+        titleTextStyle: {
+          color: '#000000',
+          fontSize: 13,
+          bold: true,
+          italic: false,
+        },
+        format: '####',
+      },
+      vAxis: {
+        title: 'Performance Score',
+        titleTextStyle: {
+          color: '#000000',
+          fontSize: 13,
+          bold: true,
+          italic: false,
+        },
+        baselineColor: '#d6dce0',
+        minValue: 0,
+        maxValue: 10,
+        ticks: [0, 2, 4, 6, 8, 10],
+      },
+      pointSize: 5,
+      colors: ['#2c6df9'],
+      curveType: 'function',
+      legend: 'none',
+      tooltip: { isHtml: true },
+      chartArea: { left: '15%', top: '10%' },
     };
 
     return (
       <div className={styles.careerPath}>
         <Card className={styles.careerPath_card} title="Career Path" extra={this.renderExtraCard()}>
-          <Row justify="space-between" align="middle">
-            <div className={styles.careerPath_chart}>
+          <div className={styles.careerPath_chart}>
+            <div id="divToPDF">
               <Chart
                 className={styles.chart}
                 width="500px"
                 height="300px"
                 chartType="LineChart"
                 loader={<div>Loading Chart</div>}
-                columns={[
-                  { type: 'number', label: 'Year' },
-                  { type: 'number', label: 'Performance Score' },
-                  { type: 'boolean', role: 'certainty' },
-                  { type: 'string', role: 'tooltip', p: { html: true } },
+                data={[
+                  [
+                    { type: 'number', label: 'Year' },
+                    { type: 'number', label: 'Performance Score' },
+                    { type: 'boolean', role: 'certainty' },
+                    { type: 'string', role: 'tooltip', p: { html: true } },
+                  ],
+                  [2018, 4, true, this.createCustomHTMLContent('30%', 'Manager', 'PM')],
+                  [2019, 5, true, this.createCustomHTMLContent('20%', 'Manager', 'PM')],
+                  [2020, 7, true, this.createCustomHTMLContent('10%', 'Manager', 'PM')],
+                  [2021, 2, false, this.createCustomHTMLContent('15%', 'Manager', 'PM')],
                 ]}
-                rows={[
-                  {
-                    c: [
-                      { v: 2018 },
-                      { v: 4 },
-                      { v: true },
-                      this.createCustomHTMLContent('20%', 'Manager', 'PM'),
-                    ],
-                  },
-                  {
-                    c: [
-                      { v: 2019 },
-                      { v: 5 },
-                      { v: true },
-                      this.createCustomHTMLContent('30%', 'Manager', 'PM'),
-                    ],
-                  },
-                  {
-                    c: [
-                      { v: 2020 },
-                      { v: 7 },
-                      { v: true },
-                      this.createCustomHTMLContent('40%', 'Manager', 'PM'),
-                    ],
-                  },
-                  {
-                    c: [
-                      { v: 2021 },
-                      { v: 8 },
-                      { v: false },
-                      this.createCustomHTMLContent('50%', 'Manager', 'PM'),
-                    ],
-                  },
-                ]}
-                options={{
-                  hAxis: {
-                    title: 'Duration',
-                    titleTextStyle: {
-                      color: '#000000',
-                      fontSize: 13,
-                      bold: true,
-                      italic: false,
-                    },
-                    format: '####',
-                  },
-                  vAxis: {
-                    title: 'Performance Score',
-                    titleTextStyle: {
-                      color: '#000000',
-                      fontSize: 13,
-                      bold: true,
-                      italic: false,
-                    },
-                    gridlines: {
-                      color: 'transparent',
-                    },
-                    minValue: 0,
-                    maxValue: 10,
-                    ticks: [0, 2, 4, 6, 8, 10],
-                  },
-                  pointSize: 5,
-                  colors: ['#2c6df9'],
-                  curveType: 'function',
-                  legend: 'none',
-                  tooltip: { isHtml: true },
-                }}
+                options={options}
+                chartEvents={[chartEvents]}
                 rootProps={{ 'data-testid': '1' }}
               />
             </div>
-            <div className={styles.careerPath_score}>
-              <div className={styles.careerPath_scoreWrapper}>
-                <span>{dummyData.averageScore}</span>
-                <p>Average score</p>
-              </div>
-            </div>
-          </Row>
+          </div>
+          <div className={styles.careerPath_score}>
+            <span>{dummyData.averageScore}</span>
+            <p>Average score</p>
+          </div>
         </Card>
       </div>
     );
