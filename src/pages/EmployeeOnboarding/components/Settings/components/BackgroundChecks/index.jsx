5670c5417ea4658171ca10355a11faca3e251ec1
Lam Nguyen <liam.nguyen@terralogic.com>
2020-09-30 16:04:27 +0700

    Add layout for header

--- src/pages/EmployeeOnboarding/components/Settings/components/BackgroundChecks/index.jsx
+++ src/pages/EmployeeOnboarding/components/Settings/components/BackgroundChecks/index.jsx
@@ -1,8 +1,22 @@
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable no-undef */
 import React, { Component } from 'react';
+
+import styles from './index.less';
 
 class BackgroundChecks extends Component {
   render() {
-    return <div>BackgroundChecks</div>;
+    return (
+      <div className={styles.BackgroundChecks}>
+        <div className={styles.BackgroundChecks_title}>Background checks</div>
+        <div className={styles.BackgroundChecks_subTitle}>
+          Run background checks on new and existing workers. <a href="#">Learn More</a>
+        </div>
+        <div className={styles.BackgroundChecks_forms}>
+          <p className={styles.subTitle}>General Settings</p>
+        </div>
+      </div>
+    );
   }
 }
 
