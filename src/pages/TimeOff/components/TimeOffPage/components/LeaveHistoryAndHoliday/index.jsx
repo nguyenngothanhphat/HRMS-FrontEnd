1d8cbc3d3d61943dada6b474d99e2c7a1e9dea38
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-10-13 16:48:04 +0700

    add holiday list

--- src/pages/TimeOff/components/TimeOffPage/components/LeaveHistoryAndHoliday/index.jsx
+++ src/pages/TimeOff/components/TimeOffPage/components/LeaveHistoryAndHoliday/index.jsx
@@ -2,27 +2,56 @@ import React, { PureComponent } from 'react';
 import { Tabs } from 'antd';
 import CalendarIcon from '@/assets/calendar_icon.svg';
 import ListIcon from '@/assets/list_icon.svg';
+import Holiday from './components/Holiday';
+
 import styles from './index.less';
 
 const { TabPane } = Tabs;
 
 export default class LeaveHistoryAndHoliday extends PureComponent {
-  operations = () => (
-    <div className={styles.menu}>
-      <img src={ListIcon} alt="list" />
-      <img src={CalendarIcon} alt="calendar" />
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeShowType: 1, // 1: list, 2: calendar
+    };
+  }
+
+  handleSelectShowType = (value) => {
+    this.setState({
+      activeShowType: value,
+    });
+  };
+
+  operations = () => {
+    const { activeShowType } = this.state;
+    return (
+      <div className={styles.menu}>
+        <img
+          src={ListIcon}
+          className={activeShowType === 1 ? styles.activeShowType : ''}
+          onClick={() => this.handleSelectShowType(1)}
+          alt="list"
+        />
+        <img
+          src={CalendarIcon}
+          className={activeShowType === 2 ? styles.activeShowType : ''}
+          onClick={() => this.handleSelectShowType(2)}
+          alt="calendar"
+        />
+      </div>
+    );
+  };
 
   render() {
+    const { activeShowType } = this.state;
     return (
       <div className={styles.LeaveHistoryAndHoliday}>
-        <Tabs defaultActiveKey="1" tabBarExtraContent={this.operations()}>
+        <Tabs defaultActiveKey="2" tabBarExtraContent={this.operations()}>
           <TabPane tab=" Request History" key="1">
             Request History
           </TabPane>
           <TabPane tab="Holiday" key="2">
-            Holiday
+            {activeShowType === 1 ? <Holiday /> : ''}
           </TabPane>
         </Tabs>
       </div>
