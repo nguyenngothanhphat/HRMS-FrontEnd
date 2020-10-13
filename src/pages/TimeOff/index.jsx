879761a67b495125151346d8107fb23a77c38ec0
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-10-13 10:31:00 +0700

    added route and page

---
+++ src/pages/TimeOff/index.jsx
@@ -0,0 +1,20 @@
+import React, { PureComponent } from 'react';
+import { Affix } from 'antd';
+import { PageContainer } from '@/layouts/layout/src';
+import styles from './index.less';
+
+export default class TimeOff extends PureComponent {
+  render() {
+    return (
+      <PageContainer>
+        <div className={styles.containerTimeOff}>
+          <Affix offsetTop={40}>
+            <div className={styles.titlePage}>
+              <p className={styles.titlePage__text}>Time Off</p>
+            </div>
+          </Affix>
+        </div>
+      </PageContainer>
+    );
+  }
+}
