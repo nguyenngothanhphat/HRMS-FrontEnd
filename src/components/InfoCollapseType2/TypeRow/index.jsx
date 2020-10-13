c40fa8f1db12bc252559f1be0f975de8d8b13522
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-10-13 08:54:23 +0700

    updated upload icon

--- src/components/InfoCollapseType2/TypeRow/index.jsx
+++ src/components/InfoCollapseType2/TypeRow/index.jsx
@@ -1,12 +1,14 @@
 import React, { useState, PureComponent } from 'react';
 import { Collapse, Row, Col, Menu, Dropdown } from 'antd';
-import { UploadOutlined, EllipsisOutlined } from '@ant-design/icons';
+import { EllipsisOutlined } from '@ant-design/icons';
 import PDFIcon from '@/assets/pdf_icon.png';
 import ImageIcon from '@/assets/image_icon.png';
+import UploadIcon from '@/assets/upload_icon.png';
 import DownloadIcon from '@/assets/download_icon.svg';
 import DownArrowIcon from '@/assets/downArrow.svg';
 import UpArrowIcon from '@/assets/upArrow.svg';
 import DownloadFile from '@/components/DownloadFile';
+
 import styles from './index.less';
 
 const { Panel } = Collapse;
@@ -40,8 +42,8 @@ const CollapseRow = (props) => {
   const statusAndButtons = () => (
     <div onClick={(event) => event.stopPropagation()} className={styles.statusAndButtons}>
       <a>Complete</a>
-      <div onClick={handleUploadClick}>
-        <UploadOutlined className={styles.uploadButton} />
+      <div onClick={handleUploadClick} className={styles.uploadButton}>
+        <img src={UploadIcon} alt="upload" />
       </div>
       <Dropdown overlay={menu}>
         <EllipsisOutlined onClick={handleMenuClick} className={styles.menuButton} />
