62ff3eee2f563b2eec8310dc64a4128c94a91573
INI\anh.nguyen <anh.nguyen@terralogic.com>
2020-10-14 16:55:33 +0700

    up

--- src/pages/EmployeeOnboarding/components/CustomFields/components/CreateNewField/DraggerUpload/index.jsx
+++ src/pages/EmployeeOnboarding/components/CustomFields/components/CreateNewField/DraggerUpload/index.jsx
@@ -1,35 +1,51 @@
 import React, { PureComponent } from 'react';
-import { Upload, message } from 'antd';
-import imageNone from '@/assets/MaskGroup-customField.svg';
+import { Upload, message, Button } from 'antd';
+import { connect } from 'umi';
+import { UploadOutlined } from '@ant-design/icons';
 
+@connect()
 class DraggerUpLoad extends PureComponent {
+  beforeUpload = (file) => {
+    const checkType =
+      file.type === 'image/jpeg' || file.type === 'image/png' || file.type === 'application/pdf';
+    if (!checkType) {
+      message.error('You can only upload JPG/PNG/PDF file!');
+    }
+    const isLt5M = file.size / 1024 / 1024 < 5;
+    if (!isLt5M) {
+      message.error('Image must smaller than 5MB!');
+    }
+    return checkType && isLt5M;
+  };
+
+  handleUpload = (file) => {
+    const { dispatch, getResponse = () => {} } = this.props;
+
+    const formData = new FormData();
+    formData.append('uri', file);
+    dispatch({
+      type: 'upload/uploadFile',
+      payload: formData,
+    }).then((resp) => {
+      getResponse(resp);
+    });
+  };
+
   render() {
-    const { Dragger } = Upload;
     const props = {
       name: 'file',
-      multiple: false,
-      showUploadList: false,
-      action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-      onChange(info) {
-        const { status } = info.file;
-        if (status !== 'uploading') {
-          console.log(info.file, info.fileList);
-        }
-        if (status === 'done') {
-          message.success(`${info.file.name} file uploaded successfully.`);
-        } else if (status === 'error') {
-          message.error(`${info.file.name} file upload failed.`);
-        }
-      },
+      multiple: true,
+      showUploadList: true,
     };
     return (
       <div>
-        <Dragger {...props}>
-          <p className="ant-upload-drag-icon">
-            <img src={imageNone} alt="imageNone" />
-          </p>
-          <p className="ant-upload-text">Drag you file here, or browse</p>
-        </Dragger>
+        <Upload
+          {...props}
+          beforeUpload={this.beforeUpload}
+          action={(file) => this.handleUpload(file)}
+        >
+          <Button icon={<UploadOutlined />}>Click to Upload</Button>
+        </Upload>
       </div>
     );
   }
