55f86441b2138ef8bc6e32d379c34991db2c6513
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-10-15 16:53:03 +0700

    update document admin cp with api

--- src/models/documentsManagement.js
+++ src/models/documentsManagement.js
@@ -1,41 +1,26 @@
 import { dialog } from '@/utils/utils';
-import {
-  getListDocumentsActive,
-  getListDocumentsInActive,
-  getDocumentDetail,
-} from '../services/documentsManagement';
+import { getListDocuments, getDocumentDetail } from '../services/documentsManagement';
 
 const documentsManagement = {
   namespace: 'documentsManagement',
   state: {
-    listDocumentsActive: [],
-    listDocumentsInActive: [],
+    listDocuments: [],
   },
   effects: {
-    *fetchListDocumentsActive(_, { call, put }) {
+    *fetchListDocuments(_, { call, put }) {
       try {
-        const response = yield call(getListDocumentsActive);
-        const { statusCode, data: listDocumentsActive = [] } = response;
+        const response = yield call(getListDocuments);
+        const { statusCode, data: listDocuments = [] } = response;
         if (statusCode !== 200) throw response;
-        yield put({ type: 'listDocumentsActive', payload: { listDocumentsActive } });
-      } catch (errors) {
-        dialog(errors);
-      }
-    },
-    *fetchListDocumentsInActive(_, { call, put }) {
-      try {
-        const response = yield call(getListDocumentsInActive);
-        const { statusCode, data: listDocumentsInActive = [] } = response;
-        if (statusCode !== 200) throw response;
-        yield put({ type: 'listDocumentsInActive', payload: { listDocumentsInActive } });
+        yield put({ type: 'listDocuments', payload: { listDocuments } });
       } catch (errors) {
         dialog(errors);
       }
     },
 
-    *fetchDocumentDetail({ payload: documentId = '' }, { call, put }) {
+    *fetchDocumentDetail({ payload: id = '' }, { call, put }) {
       try {
-        const response = yield call(getDocumentDetail, { documentId });
+        const response = yield call(getDocumentDetail, { id });
         const { statusCode, data: listDocumentDetail = [] } = response;
         if (statusCode !== 200) throw response;
         yield put({ type: 'listDocumentDetail', payload: { listDocumentDetail } });
@@ -45,13 +30,7 @@ const documentsManagement = {
     },
   },
   reducers: {
-    listDocumentsActive(state, action) {
-      return {
-        ...state,
-        ...action.payload,
-      };
-    },
-    listDocumentsInActive(state, action) {
+    listDocuments(state, action) {
       return {
         ...state,
         ...action.payload,
--- src/services/documentsManagement.js
+++ src/services/documentsManagement.js
@@ -1,87 +1,14 @@
-const dataActive = [
-  {
-    documentId: 'DOC 01',
-    documentType: 'Test 1',
-    documentName: 'Here is the name 1',
-    url: '/assets/files/sample_1.pdf',
-    uploadedBy: 'Terralogic',
-    createdDate: '08/29/2020',
-    userId: '5443',
-  },
-  {
-    documentId: 'DOC 02',
-    documentType: 'Test 2',
-    documentName: 'Here is the name 2',
-    url: '/assets/files/sample_2.pdf',
-    uploadedBy: 'Terralogic',
-    createdDate: '08/29/2020',
-    userId: '2343',
-  },
-  {
-    documentId: 'DOC 03',
-    documentType: 'Test 3',
-    documentName: 'Here is the name 3',
-    url: '/assets/files/sample_1.pdf',
-    uploadedBy: 'Terralogic',
-    createdDate: '08/29/2020',
-    userId: '4343',
-  },
-  {
-    documentId: 'DOC 04',
-    documentType: 'Test 4',
-    documentName: 'Here is the name 4',
-    url: '/assets/files/sample_2.pdf',
-    uploadedBy: 'Terralogic',
-    createdDate: '08/29/2020',
-    userId: '8097',
-  },
-];
+import request from '@/utils/request';
 
-const dataInActive = [
-  {
-    documentId: 'DOC 05',
-    documentType: 'Test 5',
-    documentName: 'Here is the name 5',
-    url: '/assets/files/sample_1.pdf',
-    uploadedBy: 'Terralogic',
-    createdDate: '08/29/2020',
-    userId: '5343',
-  },
-  {
-    documentId: 'DOC 06',
-    documentType: 'Test 6',
-    documentName: 'Here is the name 6',
-    url: '/assets/files/sample_2.pdf',
-    uploadedBy: 'Terralogic',
-    createdDate: '08/29/2020',
-    userId: '5423',
-  },
-];
-export const getListDocumentsActive = async () => {
-  return {
-    statusCode: 200,
-    data: dataActive,
-  };
-};
-export const getListDocumentsInActive = async () => {
-  return {
-    statusCode: 200,
-    data: dataInActive,
-  };
+export const getListDocuments = async () => {
+  return request('/api/document/list', {
+    method: 'POST',
+  });
 };
 
-export const getDocumentDetail = async (params) => {
-  let i = 0;
-  for (i; i < dataActive.length; i += 1) {
-    if (dataActive[i].documentId === params.documentId) {
-      return {
-        statusCode: 200,
-        data: dataActive[i],
-      };
-    }
-  }
-  return {
-    statusCode: 200,
-    data: [],
-  };
+export const getDocumentDetail = (payload) => {
+  return request('/api/document/get-by-id', {
+    method: 'POST',
+    data: payload,
+  });
 };
