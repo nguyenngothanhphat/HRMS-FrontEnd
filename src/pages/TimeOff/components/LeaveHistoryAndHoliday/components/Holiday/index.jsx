5194f6e216dca7ae924cd0f435f79c57d34717ed
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-10-15 09:13:36 +0700

    fix padding, margin

--- src/pages/TimeOff/components/LeaveHistoryAndHoliday/components/Holiday/index.jsx
+++ src/pages/TimeOff/components/LeaveHistoryAndHoliday/components/Holiday/index.jsx
@@ -33,11 +33,13 @@ export default class Holiday extends PureComponent {
         {mockData.map((row) => {
           return (
             <Row className={styles.eachRow}>
-              <Col xs={5} className={styles.dateAndMonth}>
+              <Col xs={4} className={styles.dateAndMonth}>
                 <span>{moment(row.time).locale('en').format('MMMM')}</span>
                 <span>{moment(row.time).locale('en').format('DD')}</span>
               </Col>
-              <Col xs={14}>{row.name}</Col>
+              <Col xs={15} className={styles.eventOfDay}>
+                {row.name}
+              </Col>
               <Col className={styles.dayInWeek} xs={5}>
                 {moment(row.time).locale('en').format('dddd')}
               </Col>
