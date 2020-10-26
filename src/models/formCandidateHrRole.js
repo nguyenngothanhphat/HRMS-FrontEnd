7c5f77dd4f695fd5a395dc148abfda774ae18e9e
Nguyenongterralogic <nguyen.ong@terralogic.com>
2020-10-26 09:10:42 +0700

    update

--- src/models/formCandidateHrRole.js
+++ src/models/formCandidateHrRole.js
@@ -373,11 +373,10 @@ const candidateInfo = {
       try {
         const response = yield call(getById, payload);
         const { data, statusCode } = response;
-        const dataObj = data.find((x) => x);
         if (statusCode !== 200) throw response;
         yield put({
           type: 'saveOrigin',
-          payload: { ...dataObj, candidate: dataObj._id, _id: dataObj._id },
+          payload: { ...data, candidate: data._id, _id: data._id },
         });
       } catch (error) {
         dialog(error);
