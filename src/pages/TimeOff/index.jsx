976fe223ab5fb08dc78cae52684ee75803e1135f
Phuoc Nguyen <phuoc.nguyen@terralogic.com>
2020-10-29 00:11:11 +0700

    Update ui

--- src/pages/TimeOff/index.jsx
+++ src/pages/TimeOff/index.jsx
@@ -93,18 +93,6 @@ export default class TimeOff extends PureComponent {
           <TabPane tab="Setup Timeoff policy" key="setupTimeOff">
             {this._renderSetupTimeoff()}
           </TabPane>
-          <TabPane tab="Setup Timeoff policy" key="setupTimeOff1">
-            <div>123412</div>
-          </TabPane>
-          <TabPane tab="Setup Timeoff policy" key="setupTimeOff2">
-            <div>123412</div>
-          </TabPane>
-          <TabPane tab="Setup Timeoff policy" key="setupTimeOff2">
-            <div>123412</div>
-          </TabPane>
-          <TabPane tab="Setup Timeoff policy" key="setupTimeOff3">
-            <div>123412</div>
-          </TabPane>
         </Tabs>
       </PageContainer>
     );
