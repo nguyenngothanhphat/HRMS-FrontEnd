1633ec81a9a52f964d2d62b2843e9c4268a7bdbf
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-11-03 13:37:11 +0700

    update header of options

--- src/pages_admin/TimeOffManagement/components/OptionsHeader/index.jsx
+++ src/pages_admin/TimeOffManagement/components/OptionsHeader/index.jsx
@@ -1,33 +1,78 @@
 import React, { PureComponent } from 'react';
-import { Row, Col, Form, Select, DatePicker } from 'antd';
+import { Row, Col, Form, Select, DatePicker, Button, Checkbox } from 'antd';
 import styles from './index.less';
 
 const { Option } = Select;
 export default class OptionsHeader extends PureComponent {
+  onFinish = (values) => {
+    console.log('values', values);
+  };
+
   render() {
     const dateFormat = 'Do MMM YYYY';
     return (
       <div className={styles.OptionsHeader}>
         <div className={styles.container}>
-          <Form name="uploadForm" ref={this.formRef}>
+          <Form name="uploadForm" ref={this.formRef} onFinish={this.onFinish}>
             <Row gutter={['20', '20']}>
               <Col xs={5}>
-                <Form.Item label="User ID - Name" name="userIdName">
+                <span className={styles.itemLabel}>User ID - Name</span>
+                <Form.Item name="userIdName">
                   <Select onChange={() => {}}>
                     <Option value="Company A">User A</Option>
                     <Option value="Company B">User B</Option>
                   </Select>
                 </Form.Item>
               </Col>
+              <Col xs={9}>
+                <span className={styles.itemLabel}>Duration</span>
+                <div>
+                  <Row gutter={['20', '20']}>
+                    <Col xs={12}>
+                      <Form.Item name="durationFrom">
+                        <DatePicker placeholder="From Date" format={dateFormat} />
+                      </Form.Item>
+                    </Col>
+                    <Col xs={12}>
+                      <span />
+                      <Form.Item name="durationTo">
+                        <DatePicker placeholder="To Date" format={dateFormat} />
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </div>
+              </Col>
+
               <Col xs={5}>
-                <Form.Item label="Duration" name="duration">
-                  <DatePicker placeholder="From Date" format={dateFormat} />
+                <span className={styles.itemLabel}>Status</span>
+                <Form.Item name="status">
+                  <Checkbox.Group style={{ width: '100%' }}>
+                    <Row>
+                      <Col span={12}>
+                        <Checkbox value="Approved">Approved</Checkbox>
+                      </Col>
+                      <Col span={12}>
+                        <Checkbox value="New">New</Checkbox>
+                      </Col>
+                      <Col span={12}>
+                        <Checkbox value="Rejected">Rejected</Checkbox>
+                      </Col>
+                      <Col span={12}>
+                        <Checkbox value="Waiting for approve">Waiting for approve</Checkbox>
+                      </Col>
+                    </Row>
+                  </Checkbox.Group>
                 </Form.Item>
               </Col>
               <Col xs={5}>
-                <Form.Item label="" name="duration">
-                  <DatePicker placeholder="To Date" format={dateFormat} />
-                </Form.Item>
+                <Row gutter={['20', '20']}>
+                  <Col xs={24}>
+                    <Button htmlType="submit">Get data</Button>
+                  </Col>
+                  <Col xs={24}>
+                    <Button>Download as CSV</Button>
+                  </Col>
+                </Row>
               </Col>
             </Row>
           </Form>
