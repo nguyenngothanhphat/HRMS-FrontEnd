13e077e8d3ca5421bdc320e29dff27bf4cabf69f
Lam Nguyen <liam.nguyen@terralogic.com>
2020-11-05 14:41:04 +0700

    Edit permisson

--- config/routes.js
+++ config/routes.js
@@ -226,7 +226,7 @@ const routes = [
             name: 'employeeOnboarding',
             icon: '/assets/images/onboarding.svg',
             component: './EmployeeOnboarding',
-            authority: ['P_ONBOARDING_VIEW'],
+            // authority: ['P_ONBOARDING_VIEW'],
           },
           {
             path: '/time-off',
