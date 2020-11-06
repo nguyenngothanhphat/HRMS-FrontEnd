f369e504bff7fb5c6037a3e344bea6840ef4e35c
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-11-06 15:49:41 +0700

    update

--- src/pages/FormTeamMember/components/BackgroundCheck/components/CollapseFields/index.jsx
+++ src/pages/FormTeamMember/components/BackgroundCheck/components/CollapseFields/index.jsx
@@ -5,94 +5,7 @@ import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
 import InputField from '../InputField';
 import styles from './index.less';
 
-const RenderNewEmployerDetail = ({ propsData, orderNumber }) => {
-  const {
-    item = {},
-    onValuesChange = () => {},
-    documentChecklistSetting,
-    checkedArr,
-    defaultArr,
-    identityProof,
-    handleChange,
-    addressProof,
-    educational,
-    processStatus,
-    handleValidation,
-    checkValidation,
-    poe,
-  } = propsData;
-  return (
-    <div>
-      {item.type === 'D' ? (
-        <InputField
-          onValuesChange={onValuesChange}
-          orderNumber={orderNumber}
-          documentChecklistSetting={documentChecklistSetting}
-          processStatus={processStatus}
-          handleValidation={handleValidation}
-          checkValidation={checkValidation}
-        />
-      ) : (
-        <></>
-      )}
-      <Space direction="vertical">
-        {checkedArr.map((data) => (
-          <Checkbox
-            checked="true"
-            disabled="true"
-            value={data.alias}
-            className={styles.checkboxItem}
-          >
-            {data.alias}
-          </Checkbox>
-        ))}
-
-        <Checkbox.Group
-          direction="vertical"
-          className={styles.checkboxItem}
-          options={defaultArr.map((data) => data.alias)}
-          onChange={(checkedList) => handleChange(checkedList, defaultArr, item)}
-          disabled={processStatus === 'SENT-PROVISIONAL-OFFER'}
-          value={
-            item.type === 'A'
-              ? identityProof.checkedList
-              : item.type === 'B'
-              ? addressProof.checkedList
-              : item.type === 'C'
-              ? educational.checkedList
-              : item.type === 'D'
-              ? poe.checkedList
-              : []
-          }
-        />
-      </Space>
-    </div>
-  );
-};
 class CollapseField extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      children: [],
-      orderNumber: 1,
-    };
-  }
-
-  addNewEmployerDetail = (propsData) => {
-    const { children, orderNumber } = this.state;
-    const newComponents = [
-      ...children,
-      <div>
-        <hr />
-        <RenderNewEmployerDetail propsData={propsData} orderNumber={orderNumber + 1} />
-      </div>,
-    ];
-    this.setState({
-      children: newComponents,
-      orderNumber: orderNumber + 1,
-    });
-  };
-
   render() {
     const {
       item = {},
@@ -102,8 +15,9 @@ class CollapseField extends PureComponent {
       onValuesChange,
       documentChecklistSetting,
       processStatus,
+      handleValidation,
+      checkValidation,
     } = this.props;
-    const { children } = this.state;
     const { identityProof, addressProof, educational, technicalCertification } = tempData;
     const { poe } = technicalCertification;
     const checkedArr = item.data
@@ -118,7 +32,6 @@ class CollapseField extends PureComponent {
           )
         : null
       : null;
-
     const defaultArr = item.data.filter(
       (data) =>
         data.key !== 'aadharCard' &&
@@ -127,21 +40,6 @@ class CollapseField extends PureComponent {
         data.key !== 'intermediateDiploma' &&
         data.key !== 'graduation',
     );
-
-    const renderProps = {
-      item,
-      documentChecklistSetting,
-      checkedArr,
-      defaultArr,
-      identityProof,
-      handleChange,
-      addressProof,
-      educational,
-      poe,
-      onValuesChange,
-      addNewEmployerDetail: this.addNewEmployerDetail,
-    };
-
     return (
       <div className={styles.CollapseField}>
         <Collapse
@@ -175,16 +73,56 @@ class CollapseField extends PureComponent {
             }
             extra="[Can submit any of the below other than (*)mandatory]"
           >
-            <RenderNewEmployerDetail propsData={renderProps} orderNumber={1} />
-            <div>{children.map((child) => child)}</div>
             {item.type === 'D' ? (
-              <Space direction="horizontal" onClick={() => this.addNewEmployerDetail(renderProps)}>
-                <PlusOutlined className={styles.plusIcon} />
-                <Typography.Text className={styles.addMore}>Add Employer Details</Typography.Text>
-              </Space>
+              <InputField
+                onValuesChange={onValuesChange}
+                documentChecklistSetting={documentChecklistSetting}
+                processStatus={processStatus}
+                handleValidation={handleValidation}
+                checkValidation={checkValidation}
+              />
             ) : (
               <></>
             )}
+            <Space direction="vertical">
+              {checkedArr.map((data) => (
+                <Checkbox
+                  checked="true"
+                  disabled="true"
+                  value={data.alias}
+                  className={styles.checkboxItem}
+                >
+                  {data.alias}
+                </Checkbox>
+              ))}
+
+              <Checkbox.Group
+                direction="vertical"
+                className={styles.checkboxItem}
+                options={defaultArr.map((data) => data.alias)}
+                onChange={(checkedList) => handleChange(checkedList, defaultArr, item)}
+                disabled={processStatus === 'SENT-PROVISIONAL-OFFER'}
+                value={
+                  item.type === 'A'
+                    ? identityProof.checkedList
+                    : item.type === 'B'
+                    ? addressProof.checkedList
+                    : item.type === 'C'
+                    ? educational.checkedList
+                    : item.type === 'D'
+                    ? poe.checkedList
+                    : []
+                }
+              />
+              {item.type === 'D' ? (
+                <Space direction="horizontal">
+                  <PlusOutlined className={styles.plusIcon} />
+                  <Typography.Text className={styles.addMore}>Add Employer Details</Typography.Text>
+                </Space>
+              ) : (
+                <></>
+              )}
+            </Space>
           </Collapse.Panel>
         </Collapse>
       </div>
