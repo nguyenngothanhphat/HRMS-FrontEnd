3927c0b3fd4ddd72845aa0119e1a7e2048c2d6a1
INI\anh.nguyen <anh.nguyen@terralogic.com>
2020-11-09 10:21:45 +0700

    Fix bug

--- config/routes.js
+++ config/routes.js
@@ -248,7 +248,7 @@ const routes = [
             name: 'employeeOnboarding',
             icon: '/assets/images/onboarding.svg',
             component: './EmployeeOnboarding',
-            authority: ['P_ONBOARDING_VIEW'],
+            authority: ['hr'],
           },
           {
             path: '/employee-onboarding/CreateFieldSection',
