985c37d8e9b1df9e46d2d25fa3f8d0ed3c6e328a
phat nguyen <phat.nguyen@terralogic.com>
2020-11-11 09:50:35 +0700

    improve UI

--- src/pages/EmployeeOnboarding/components/OnboardingOverview/index.jsx
+++ src/pages/EmployeeOnboarding/components/OnboardingOverview/index.jsx
@@ -1,10 +1,13 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'umi';
+import { Spin } from 'antd';
 import OnboardingLayout from '@/components/OnboardingLayout';
 import OnboardingEmpty from './components/OnboardingEmpty';
+import styles from './index.less';
 
-@connect(({ onboard: { menu = {} } = {} }) => ({
+@connect(({ onboard: { menu = {} } = {}, loading }) => ({
   menu,
+  loading: loading.effects['onboard/fetchOnboardList'],
 }))
 class OnboardingOverview extends Component {
   componentDidMount() {
@@ -19,13 +22,18 @@ class OnboardingOverview extends Component {
   }
 
   render() {
-    const { menu: { onboardingOverviewTab: { listMenu = [] } = {} } = {} } = this.props;
-    const checkEmpty = listMenu.map((item) => item.quantity).reduce((a, b) => a + b, 0);
-    return (
-      <Fragment>
-        {checkEmpty === 0 ? <OnboardingEmpty /> : <OnboardingLayout listMenu={listMenu} />}
-      </Fragment>
-    );
+    const {
+      menu: { onboardingOverviewTab: { listMenu = [] } = {} } = {},
+      loading = true,
+    } = this.props;
+    const checkEmpty = !loading && listMenu.map((item) => item.quantity).reduce((a, b) => a + b, 0);
+    if (!checkEmpty)
+      return (
+        <div className={styles.loading}>
+          <Spin size="large" />
+        </div>
+      );
+    return checkEmpty === 0 ? <OnboardingEmpty /> : <OnboardingLayout listMenu={listMenu} />;
   }
 }
 
