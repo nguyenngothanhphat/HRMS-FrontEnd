ef07eaad866668a19fa4a4cd8c4c2d2e37536980
phat nguyen <phat.nguyen@terralogic.com>
2020-11-11 11:08:49 +0700

    re-order menu, add icon add team member in button at page onboarding empty

--- src/pages/EmployeeOnboarding/components/OnboardingOverview/components/OnboardingEmpty/index.jsx
+++ src/pages/EmployeeOnboarding/components/OnboardingOverview/components/OnboardingEmpty/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Button } from 'antd';
 import { formatMessage, connect } from 'umi';
 import s from './index.less';
@@ -21,7 +21,14 @@ class Empty extends Component {
         <img src="/assets/images/expand-your-team.png" alt="iconCheck" className={s.imgEmpty} />
         <div className={s.textEmpty}>No members have been onboarded yet. Expand your team!</div>
         <Button className={s.btnAddMember} onClick={this.handleAddTeamMember} loading={loading}>
-          {formatMessage({ id: 'component.onboardingOverview.addTeamMember' })}
+          <Fragment>
+            <img
+              src="/assets/images/add-team-member.png"
+              alt="add member icon"
+              style={{ width: '20px', height: '20px', marginRight: '10px' }}
+            />
+            <span>{formatMessage({ id: 'component.onboardingOverview.addTeamMember' })}</span>
+          </Fragment>
         </Button>
       </div>
     );
