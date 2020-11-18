809d87fb82111fbcd6a237698c1445859d1129fd
INI\phuong.van <phuong.van@terralogic.com>
2020-11-18 12:55:49 +0700

    Init Relieving Formalities

---
+++ src/pages/OffBoarding/HrOffboarding/component/RelievingFormalities/components/RelievingDetail/index.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import styles from './index.less';
+
+class RelievingDetail extends PureComponent {
+  render() {
+    return (
+      <div className={styles.relievingDetail}>
+        <p>Relieving Detail</p>
+      </div>
+    );
+  }
+}
+
+export default RelievingDetail;
---
+++ src/pages/OffBoarding/HrOffboarding/component/RelievingFormalities/index.jsx
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import styles from './index.less';
+
+class RelievingFormalities extends PureComponent {
+  render() {
+    return (
+      <div className={styles.relievingFormalities}>
+        <p style={{ padding: '24px' }}>Content Relieving Formalities</p>
+      </div>
+    );
+  }
+}
+
+export default RelievingFormalities;
