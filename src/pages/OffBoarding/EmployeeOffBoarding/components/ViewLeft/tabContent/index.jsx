fa8bae945a114f95b99f0a7c374eb363457c3437
nguyenminhduc741 <david.nguyen@terralogic.com>
2020-11-18 11:42:26 +0700

    Phat push

--- src/pages/OffBoarding/EmployeeOffBoarding/components/ViewLeft/tabContent/index.jsx
+++ src/pages/OffBoarding/EmployeeOffBoarding/components/ViewLeft/tabContent/index.jsx
@@ -1,15 +1,66 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'umi';
 import TableEmployee from '../TableEmployee';
 import RejectTable from '../RejectTable';
 import styles from './index.less';
 
-export default class TabContent extends PureComponent {
+@connect()
+class TabContent extends Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedFilterTab: '1',
     };
   }
+
+  componentDidMount() {
+    this.initDataTable('1');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    const { selectedFilterTab } = this.state;
+    const { selectedFilterTab: nextTabId } = nextState;
+    if (selectedFilterTab !== nextTabId) {
+      this.initDataTable(nextTabId);
+    }
+    return true;
+  }
+
+  initDataTable = (tabId) => {
+    const { dispatch } = this.props;
+    if (tabId === '1') {
+      dispatch({
+        type: 'offboarding/fetchList',
+        payload: {
+          status: 'IN-PROGRESS',
+        },
+      });
+    }
+    if (tabId === '2') {
+      dispatch({
+        type: 'offboarding/fetchList',
+        payload: {
+          status: 'ON-HOLD',
+        },
+      });
+    }
+    if (tabId === '3') {
+      dispatch({
+        type: 'offboarding/fetchList',
+        payload: {
+          status: 'ACCEPTED',
+        },
+      });
+    }
+    if (tabId === '4') {
+      dispatch({
+        type: 'offboarding/fetchList',
+        payload: {
+          status: 'REJECTED',
+        },
+      });
+    }
+  };
 
   setSelectedTab = (id) => {
     this.setState({
@@ -25,11 +76,13 @@ export default class TabContent extends PureComponent {
         <RejectTable setSelectedTab={this.setSelectedTab} />
         <div className={styles.tableContainer}>
           {selectedFilterTab === '1' ? <TableEmployee data={data} /> : ''}
-          {selectedFilterTab === '2' ? <TableEmployee /> : ''}
-          {selectedFilterTab === '3' ? <TableEmployee /> : ''}
-          {selectedFilterTab === '4' ? <TableEmployee /> : ''}
+          {selectedFilterTab === '2' ? <TableEmployee data={data} /> : ''}
+          {selectedFilterTab === '3' ? <TableEmployee data={data} /> : ''}
+          {selectedFilterTab === '4' ? <TableEmployee data={data} /> : ''}
         </div>
       </div>
     );
   }
 }
+
+export default TabContent;
