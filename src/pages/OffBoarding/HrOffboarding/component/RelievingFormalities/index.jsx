bd3b4ff2491e5e46162f328121d312841c38d5d2
Lam Nguyen <liam.nguyen@terralogic.com>
2020-11-18 15:39:21 +0700

    Add tables

--- src/pages/OffBoarding/HrOffboarding/component/RelievingFormalities/index.jsx
+++ src/pages/OffBoarding/HrOffboarding/component/RelievingFormalities/index.jsx
@@ -1,11 +1,22 @@
 import React, { PureComponent } from 'react';
+import { Row, Col } from 'antd';
+
+import RelievingTables from './components/RelievingTables';
 import styles from './index.less';
 
 class RelievingFormalities extends PureComponent {
   render() {
     return (
       <div className={styles.relievingFormalities}>
-        <p style={{ padding: '24px' }}>Content Relieving Formalities</p>
+        {/* <p style={{ padding: '24px' }}>Content Relieving Formalities</p> */}
+        <Row gutter={[24, 24]}>
+          <Col xs={24} sm={24} md={24} lg={18} xl={18}>
+            <RelievingTables />
+          </Col>
+          <Col xs={24} sm={24} md={24} lg={6} xl={6}>
+            8
+          </Col>
+        </Row>
       </div>
     );
   }
