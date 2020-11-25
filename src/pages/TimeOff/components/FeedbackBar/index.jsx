56c0ff0832947034a680620ad23a6b44a0b468da
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-11-25 16:39:09 +0700

    update feedback bar when clicking on like/dislike button

--- src/pages/TimeOff/components/FeedbackBar/index.jsx
+++ src/pages/TimeOff/components/FeedbackBar/index.jsx
@@ -3,27 +3,43 @@ import { LikeOutlined, DislikeOutlined, CloseOutlined } from '@ant-design/icons'
 import styles from './index.less';
 
 export default class FeedbackBar extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onLikeClick: false,
+    };
+  }
+
   onLikeClick = () => {
-    // eslint-disable-next-line no-alert
-    alert('Clicked');
+    this.setState({
+      onLikeClick: true,
+    });
   };
 
   render() {
     const { onClose = () => {} } = this.props;
+    const { onLikeClick } = this.state;
     return (
       <div className={styles.FeedbackBar}>
         <div className={styles.container}>
-          <div className={styles.leftPart}>
-            <span className={styles.title}>Are you satisfied with our Timeoff app?</span>
-            <div className={styles.likeButton} onClick={this.onLikeClick}>
-              <LikeOutlined />
-              <span className={styles.text}>Yes</span>
+          {onLikeClick ? (
+            <div className={styles.leftPart}>
+              <span className={styles.title}>Thank you for your feedback!</span>
             </div>
-            <div className={styles.likeButton} onClick={this.onLikeClick}>
-              <DislikeOutlined />
-              <span className={styles.text}>No</span>
+          ) : (
+            <div className={styles.leftPart}>
+              <span className={styles.title}>Are you satisfied with our Timeoff app?</span>
+              <div className={styles.likeButton} onClick={this.onLikeClick}>
+                <LikeOutlined />
+                <span className={styles.text}>Yes</span>
+              </div>
+              <div className={styles.likeButton} onClick={this.onLikeClick}>
+                <DislikeOutlined />
+                <span className={styles.text}>No</span>
+              </div>
             </div>
-          </div>
+          )}
+
           <div className={styles.rightPart} onClick={onClose}>
             <CloseOutlined />
           </div>
