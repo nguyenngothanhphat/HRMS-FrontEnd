7ee3c3dc42d07d6ac53dcac15735e9b4ecd8ae16
phat nguyen <phat.nguyen@terralogic.com>
2020-12-15 17:46:41 +0700

    improve UI Dashboard

---
+++ src/pages/Dashboard/components/TimeSheet/index.jsx
@@ -0,0 +1,8 @@
+import React, { PureComponent } from 'react';
+import s from './index.less';
+
+export default class TimeSheet extends PureComponent {
+  render() {
+    return <div className={s.root}>Component Time Sheet</div>;
+  }
+}
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,13 +1,32 @@
 import React, { PureComponent } from 'react';
 import { PageContainer } from '@/layouts/layout/src';
 import { Affix, Row, Col } from 'antd';
-import { ReactComponent as HomeIcon } from '@/assets/dashboard_home.svg';
 import Greeting from './components/Greeting';
 import ActivityLog from './components/ActivityLog';
 import MyApps from './components/MyApps';
-
+import TabManageTeamWork from './components/TabManageTeamWork';
+import TimeSheet from './components/TimeSheet';
+import Links from './components/Links';
 import Carousel from './components/Carousel';
 import styles from './index.less';
+
+const listLinkFQAs = [
+  { name: 'I cannot access an app?', href: '' },
+  { name: 'How do I integrate google calendar with the portal?', href: '' },
+  { name: 'I cannot access an app?', href: '' },
+  { name: 'How do I integrate google calendar with the portal?', href: '' },
+  { name: 'How do I remove an app from the list of apps?', href: '' },
+  { name: 'How do I remove an app from the list of apps?', href: '' },
+];
+
+const listQuickLinks = [
+  { name: 'Coronavirus resources', href: '', isNew: true },
+  { name: 'Work From Home guidelines', href: '', isNew: true },
+  { name: 'Employee Handbook', href: '' },
+  { name: 'Annual Report 2020', href: '' },
+  { name: 'Training Program 2020', href: '' },
+  { name: 'Submit Commuter Claim', href: '' },
+];
 
 export default class Dashboard extends PureComponent {
   render() {
@@ -19,18 +38,9 @@ export default class Dashboard extends PureComponent {
               <p className={styles.titlePage__text}>Dashboard</p>
             </div>
           </Affix>
-          <Row gutter={[24, 24]}>
+          <Row gutter={[24, 24]} style={{ padding: '20px 20px 0 0' }}>
             <Col span={7}>
-              {/* <div style={{ width: '100%', backgroundColor: 'red' }}>AAAA</div> */}
-              <button type="button" className={styles.homeIcon}>
-                <div>
-                  <HomeIcon />
-                  <span>Home</span>
-                </div>
-              </button>
-
               <Greeting />
-
               <div className={styles.leftContainer}>
                 <MyApps />
                 <div className={styles.divide} />
@@ -41,10 +51,16 @@ export default class Dashboard extends PureComponent {
               <Carousel />
               <Row gutter={[12, 12]}>
                 <Col span={12}>
-                  <div style={{ width: '100%', backgroundColor: 'red' }}>AAAAA</div>
+                  <TabManageTeamWork />
                 </Col>
                 <Col span={12}>
-                  <div style={{ width: '100%', backgroundColor: 'red' }}>AAAAA</div>
+                  <TimeSheet />
+                </Col>
+                <Col span={12}>
+                  <Links title="FAQs" showButton listData={listLinkFQAs} />
+                </Col>
+                <Col span={12}>
+                  <Links title="Quick Links" listData={listQuickLinks} />
                 </Col>
               </Row>
             </Col>
