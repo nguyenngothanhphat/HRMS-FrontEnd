635135ba40397b8a93846d4b0de5a66f1d545337
duy.tran <duy.tran@terralogic.com>
2020-12-16 10:21:53 +0700

    activity log

--- src/pages/Dashboard/components/ActivityLog/index.jsx
+++ src/pages/Dashboard/components/ActivityLog/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ReactComponent as SortIcon } from '@/assets/dashboard_sort.svg';
+import { ReactComponent as FilterIcon } from '@/assets/dashboard_filter.svg';
 import { Tabs } from 'antd';
 
 import ActivityItem from '../ActivityItem';
@@ -10,8 +12,9 @@ const { TabPane } = Tabs;
 const activityList = [
   {
     id: 1,
-    day: 23,
+    day: 22,
     month: 'May',
+    year: 2020,
     info: (
       <p>
         Resource allocation sheet for Week 17 <strong>[22nd August - 28th August, 2020]</strong>{' '}
@@ -21,8 +24,9 @@ const activityList = [
   },
   {
     id: 2,
-    day: 23,
-    month: 'May',
+    day: 15,
+    month: 'June',
+    year: 2020,
     info: (
       <p>
         Resource allocation sheet for Week 17 <strong>[22nd August - 28th August, 2020]</strong>{' '}
@@ -32,8 +36,57 @@ const activityList = [
   },
   {
     id: 3,
-    day: 23,
+    day: 4,
     month: 'May',
+    year: 2020,
+    info: (
+      <p>
+        Resource allocation sheet for Week 17 <strong>[22nd August - 28th August, 2020]</strong>{' '}
+        received.
+      </p>
+    ),
+  },
+  {
+    id: 4,
+    day: 8,
+    month: 'Jule',
+    year: 2020,
+    info: (
+      <p>
+        Resource allocation sheet for Week 17 <strong>[22nd August - 28th August, 2020]</strong>{' '}
+        received.
+      </p>
+    ),
+  },
+  {
+    id: 5,
+    day: 13,
+    month: 'Feb',
+    year: 2020,
+    info: (
+      <p>
+        Resource allocation sheet for Week 17 <strong>[22nd August - 28th August, 2020]</strong>{' '}
+        received.
+      </p>
+    ),
+  },
+  {
+    id: 6,
+    day: 17,
+    month: 'Oct',
+    year: 2020,
+    info: (
+      <p>
+        Resource allocation sheet for Week 17 <strong>[22nd August - 28th August, 2020]</strong>{' '}
+        received.
+      </p>
+    ),
+  },
+  {
+    id: 7,
+    day: 23,
+    month: 'Aug',
+    year: 2020,
     info: (
       <p>
         Resource allocation sheet for Week 17 <strong>[22nd August - 28th August, 2020]</strong>{' '}
@@ -43,22 +96,93 @@ const activityList = [
   },
 ];
 
+const allListData = activityList;
+const requireListData = activityList.splice(0, 5);
+
 const ActivityLog = () => {
+  const [ascending, setAscending] = useState(false);
+  const [allList, setAllList] = useState([]);
+  const [activeTab, setActiveTab] = useState(1);
+  const [requireList, setRequireList] = useState([]);
+
+  const allCount = allList.length < 10 ? `0${allList.length}` : allList.length;
+  const requireCount = requireList.length < 10 ? `0${requireList.length}` : requireList.length;
+
+  useEffect(() => {
+    setAllList(allListData);
+    setRequireList(requireListData);
+  }, []);
+
+  const sort = () => {
+    let currentList;
+    if (activeTab === 1) {
+      currentList = [...allList];
+    } else {
+      currentList = [...requireList];
+    }
+
+    if (currentList.length === 0) {
+      return;
+    }
+
+    // sort
+    const newList = currentList.sort((item1, item2) => {
+      const date1 = new Date(`${item1.month} ${item1.day},${item1.year}`);
+      const date2 = new Date(`${item2.month} ${item2.day},${item2.year}`);
+      console.log(date1.getTime() - date2.getTime());
+      if (ascending) {
+        return date2.getTime() - date1.getTime();
+      }
+      return date1.getTime() - date2.getTime();
+    });
+
+    // Update correspond list
+    if (activeTab === 1) {
+      setAllList(newList);
+    } else {
+      setRequireList(newList);
+    }
+
+    setAscending((prevState) => !prevState);
+  };
+
+  const filter = () => {
+    console.log('FILTER');
+  };
+
   return (
     <div className={s.container}>
       <h3>activity log</h3>
-      <Tabs defaultActiveKey="1">
-        <TabPane tab="All (11)" key="1">
-          <div style={{ margin: '4px 0' }}>
-            {/* <ActivityItem /> */}
-            {activityList.map((item) => {
+
+      <div className={s.actionContainer}>
+        <div className={s.action} onClick={() => sort()}>
+          <SortIcon />
+          <span>sort</span>
+        </div>
+
+        <div className={s.action} onClick={() => filter()}>
+          <FilterIcon />
+          <span>filter</span>
+        </div>
+      </div>
+
+      <Tabs defaultActiveKey="1" onChange={(key) => setActiveTab(key)}>
+        <TabPane tab={`All (${allCount})`} key="1">
+          <div className={s.main}>
+            {allList.map((item) => {
               const { day = '', month = '', info = '', id = '' } = item;
               return <ActivityItem key={id} day={day} month={month} info={info} />;
             })}
           </div>
         </TabPane>
-        <TabPane tab="Requires my approvals (02)" key="2">
-          b
+
+        <TabPane tab={`Requires my approvals (${requireCount})`} key="2">
+          <div className={s.main}>
+            {requireList.map((item) => {
+              const { day = '', month = '', info = '', id = '' } = item;
+              return <ActivityItem key={id} day={day} month={month} info={info} />;
+            })}
+          </div>
         </TabPane>
       </Tabs>
     </div>
