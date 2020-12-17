1a392f86000d2ddef6c4dc7a620703d8f30b014b
phat nguyen <phat.nguyen@terralogic.com>
2020-12-17 15:17:12 +0700

    add avt assigned

--- src/pages/OffBoarding/HrOffboarding/component/HrRequestTable/TableHRManager/index.jsx
+++ src/pages/OffBoarding/HrOffboarding/component/HrRequestTable/TableHRManager/index.jsx
@@ -1,10 +1,10 @@
+/* eslint-disable react/no-array-index-key */
 import React, { PureComponent } from 'react';
-import { Table, Popover, notification } from 'antd';
+import { Table, Popover, notification, Avatar } from 'antd';
 import moment from 'moment';
 import empty from '@/assets/empty.svg';
-import persion from '@/assets/people.svg';
+import { UserOutlined } from '@ant-design/icons';
 import { history } from 'umi';
-// import persion from '@/assets/people.svg';
 import styles from './index.less';
 
 class HrTable extends PureComponent {
@@ -115,18 +115,26 @@ class HrTable extends PureComponent {
       {
         title: <span className={styles.title}>Assigned </span>,
         dataIndex: 'Assigned',
-        render: () => (
-          <div className={styles.rowAction}>
-            <p>
-              <span>
-                <img src={persion} style={{ marginTop: '10px' }} alt="" />
-              </span>
-              <span>
-                <img src={persion} style={{ marginTop: '10px' }} alt="" />
-              </span>
-            </p>
-          </div>
-        ),
+        render: (_, row) => {
+          console.log('row', row);
+          const {
+            manager: { generalInfo: { avatar: avtManager = '' } = {} } = {},
+            hrManager: { generalInfo: { avatar: avtHrManager = '' } = {} } = {},
+          } = row;
+          const arrAvt = [avtManager, avtHrManager];
+          return (
+            <div className={styles.rowAction}>
+              {arrAvt.map(
+                (item, index) =>
+                  item && (
+                    <div key={index} style={{ marginRight: '13px', display: 'inline-block' }}>
+                      <Avatar src={item} size={20} icon={<UserOutlined />} />
+                    </div>
+                  ),
+              )}
+            </div>
+          );
+        },
       },
       {
         title: <span className={styles.title}>Department</span>,
