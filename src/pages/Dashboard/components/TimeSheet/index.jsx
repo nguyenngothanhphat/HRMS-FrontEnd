9a5b15124a241aa0ca09139a274d521107b27f81
phat nguyen <phat.nguyen@terralogic.com>
2020-12-17 14:36:08 +0700

    improve UI

--- src/pages/Dashboard/components/TimeSheet/index.jsx
+++ src/pages/Dashboard/components/TimeSheet/index.jsx
@@ -1,36 +1,39 @@
 import React, { PureComponent } from 'react';
 import { Calendar } from 'antd';
+import { CheckOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import s from './index.less';
 
 export default class TimeSheet extends PureComponent {
-  disabledDate = (current) => {
-    return current && current > moment().subtract(1, 'day').endOf('day');
+  getStatusDay = (value) => {
+    const disable = moment() > moment().subtract(1, 'day').endOf('day');
+    const isWeekend =
+      moment(value).locale('en').format('ddd') === 'Sat' ||
+      moment(value).locale('en').format('ddd') === 'Sun';
+    const status = 'normal';
+    return status;
   };
 
   dateFullCellRender = (value) => {
+    this.getStatusDay(value);
     const date = value.date();
     const dummy = {
       '04/12/2020': true,
       '05/12/2020': true,
       '06/12/2020': true,
       '15/12/2020': true,
-      '20/12/2020': true,
     };
     const key = moment(value).format('DD/MM/YYYY');
     const check = dummy[key] || false;
     const className = check ? `${s.check}` : `${s.notCheck}`;
-    return <div className={`${s.date} ${className}`}>{date}</div>;
+    const content = check ? <CheckOutlined className={s.iconCheck} /> : date;
+    return <div className={`${s.date} ${className}`}>{content}</div>;
   };
 
   render() {
     return (
       <div className={s.root}>
-        <Calendar
-          dateCellRender={this.dateCellRender}
-          disabledDate={this.disabledDate}
-          dateFullCellRender={this.dateFullCellRender}
-        />
+        <Calendar dateFullCellRender={this.dateFullCellRender} />
       </div>
     );
   }
