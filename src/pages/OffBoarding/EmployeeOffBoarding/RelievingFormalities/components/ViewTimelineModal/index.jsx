9b70092c6bb31507b2d135e3bd12d77dc9226507
Lewis Nguyen <lewis.nguyen@terralogic.com>
2020-12-18 13:58:37 +0700

    update calendar

--- src/pages/OffBoarding/EmployeeOffBoarding/RelievingFormalities/components/ViewTimelineModal/index.jsx
+++ src/pages/OffBoarding/EmployeeOffBoarding/RelievingFormalities/components/ViewTimelineModal/index.jsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
 import { Calendar, Select, Radio, Col, Row, Typography, Modal, Button } from 'antd';
-
+import { CheckOutlined } from '@ant-design/icons';
+import moment from 'moment';
 import { connect } from 'umi';
 import styles from './index.less';
 
@@ -24,6 +25,55 @@ class ViewTimelineModal extends PureComponent {
 
   onPanelChange = (value, mode) => {
     console.log(value, mode);
+  };
+
+  getStatusDay = (value) => {
+    const disable = moment() < moment(value).subtract(1, 'day').endOf('day');
+    const isWeekend =
+      moment(value).locale('en').format('ddd') === 'Sat' ||
+      moment(value).locale('en').format('ddd') === 'Sun';
+    const isCurrent = moment().format('DD/MM/YYYY') === moment(value).format('DD/MM/YYYY');
+
+    if (isWeekend) {
+      return 'weekend';
+    }
+    if (disable) {
+      return 'disable';
+    }
+    if (isCurrent) {
+      return 'current';
+    }
+    return 'normal';
+  };
+
+  dateFullCellRender = (value) => {
+    const date = value.date();
+    const status = this.getStatusDay(value);
+    return status === 'normal'
+      ? this.handeCheckTimeSheet(value, date)
+      : this.renderItemDay(status, date);
+  };
+
+  renderItemDay = (status, date) => {
+    return <div className={`${styles.date} ${styles[status]}`}>{date}</div>;
+  };
+
+  handeCheckTimeSheet = (value, date) => {
+    const dummy = {
+      '04/12/2020': true,
+      '08/12/2020': true,
+      '09/12/2020': true,
+      '15/12/2020': true,
+    };
+    const key = moment(value).format('DD/MM/YYYY');
+    const check = dummy[key] || false;
+    return check ? (
+      <div className={`${styles.date} ${styles.check}`}>
+        <CheckOutlined className={styles.iconCheck} />
+      </div>
+    ) : (
+      <div className={`${styles.date} ${styles.notCheck}`}>{date}</div>
+    );
   };
 
   render() {
@@ -50,12 +100,12 @@ class ViewTimelineModal extends PureComponent {
                 const current = value.clone();
                 const localeData = value.localeData();
                 const months = [];
-                for (let i = 0; i < 12; i++) {
+                for (let i = 0; i < 12; i += 1) {
                   current.month(i);
                   months.push(localeData.monthsShort(current));
                 }
 
-                for (let index = start; index < end; index++) {
+                for (let index = start; index < end; index += 1) {
                   monthOptions.push(
                     <Select.Option className="month-item" key={`${index}`}>
                       {months[index]}
@@ -121,6 +171,8 @@ class ViewTimelineModal extends PureComponent {
               }}
               fullscreen
               onPanelChange={this.onPanelChange}
+              mode="month"
+              dateFullCellRender={this.dateFullCellRender}
             />
           </div>
           <div className={styles.footer}>
