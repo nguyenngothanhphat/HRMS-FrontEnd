7f03c9e4c9ee135b62dc91e8e4a07f8e1c7ccd48
phat nguyen <phat.nguyen@terralogic.com>
2020-12-22 16:57:56 +0700

    improve UI

--- src/components/HeaderSearch/components/ViewHistory/index.jsx
+++ src/components/HeaderSearch/components/ViewHistory/index.jsx
@@ -2,7 +2,7 @@
 import React, { PureComponent } from 'react';
 import iconRecent from '@/assets/iconRecent.png';
 import { UserOutlined, AntDesignOutlined } from '@ant-design/icons';
-import { Row, Col, Avatar } from 'antd';
+import { Avatar } from 'antd';
 import s from './index.less';
 
 const dummyListRecent = ['PSI 2021', 'Townhall - December, 2020', 'Pitch doc'];
@@ -14,12 +14,20 @@ const dummyListPeople = [
   'Krithi Priyadarshini',
   'Shipra Purohit',
   'Aditya Venkatesan',
-  'Manasi Sanghani',
 ];
 
 const dummyListType = ['Documents', 'Reports', 'Requests', 'Events'];
 
 class ViewHistory extends PureComponent {
+  renderItem = (item, index, isType = false) => {
+    return (
+      <div key={index} className={s.itemDisplay}>
+        <Avatar size={40} icon={!isType ? <UserOutlined /> : <AntDesignOutlined />} />
+        <p style={{ marginTop: '10px' }}>{item}</p>
+      </div>
+    );
+  };
+
   render() {
     const { changeMode = () => {} } = this.props;
     return (
@@ -28,38 +36,18 @@ class ViewHistory extends PureComponent {
           <div className={s.blockRecent}>
             <p className={s.titleBlock}>Recent</p>
             {dummyListRecent.map((item, index) => (
-              <div key={index} style={{ marginTop: '16px' }}>
+              <div key={index} style={{ marginTop: '16px', cursor: 'pointer' }}>
                 <img src={iconRecent} alt="iconRecent" style={{ marginRight: '12px' }} />
                 {item}
               </div>
             ))}
           </div>
         )}
-        <div className={s.blockRecent}>
-          <p className={s.titleBlock} style={{ marginBottom: '16px' }}>
-            People
-          </p>
-          <Row gutter={[16, 16]}>
-            {dummyListPeople.map((item, index) => (
-              <Col span={6} key={index} className={s.viewCol}>
-                <Avatar style={{ marginRight: '10px' }} size="small" icon={<UserOutlined />} />
-                {item}
-              </Col>
-            ))}
-          </Row>
+        <div className={`${s.blockRecent} ${s.viewRow}`}>
+          {dummyListPeople.map((item, index) => this.renderItem(item, index))}
         </div>
-        <div className={s.blockRecent}>
-          <p className={s.titleBlock} style={{ marginBottom: '16px' }}>
-            Type
-          </p>
-          <Row gutter={[16, 16]}>
-            {dummyListType.map((item, index) => (
-              <Col span={6} key={index} className={s.viewCol}>
-                <Avatar style={{ marginRight: '10px' }} size="small" icon={<AntDesignOutlined />} />
-                {item}
-              </Col>
-            ))}
-          </Row>
+        <div className={`${s.blockRecent} ${s.viewRow}`}>
+          {dummyListType.map((item, index) => this.renderItem(item, index, true))}
         </div>
         <div className={s.viewActionChangeMode} onClick={() => changeMode('advanced')}>
           Advanced Search
