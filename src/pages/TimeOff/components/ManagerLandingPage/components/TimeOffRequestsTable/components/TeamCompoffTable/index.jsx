5107b09b38474ddebe540ef17ecec5d454564d59
Lewis Nguyen <lewis.nguyen@terralogic.com>
2021-01-14 10:35:38 +0700

    add approve/reject compoff request directly on the table

--- src/pages/TimeOff/components/ManagerLandingPage/components/TimeOffRequestsTable/components/TeamCompoffTable/index.jsx
+++ src/pages/TimeOff/components/ManagerLandingPage/components/TimeOffRequestsTable/components/TeamCompoffTable/index.jsx
@@ -5,6 +5,7 @@ import ApproveIcon from '@/assets/approveTR.svg';
 import OpenIcon from '@/assets/openTR.svg';
 import CancelIcon from '@/assets/cancelTR.svg';
 import moment from 'moment';
+import RejectCommentModal from '../RejectCommentModal';
 import styles from './index.less';
 
 @connect(({ loading }) => ({
@@ -99,15 +100,26 @@ class TeamCompoffTable extends PureComponent {
     {
       title: 'Action',
       align: 'left',
-      dataIndex: '_id',
-      render: (_id) => {
+      dataIndex: 'id',
+      render: (id) => {
+        const { ticketID = '', _id = '' } = id;
         const { selectedTab = '' } = this.props;
-        if (selectedTab === 'IN-PROGRESS')
+        if (selectedTab === 'IN-PROGRESS' || selectedTab === 'IN-PROGRESS-NEXT')
           return (
             <div className={styles.rowAction}>
-              <img src={OpenIcon} onClick={() => this.onOpenClick(_id)} alt="open" />
-              <img src={ApproveIcon} onClick={this.onApproveClick} alt="approve" />
-              <img src={CancelIcon} onClick={this.onCancelClick} alt="cancel" />
+              <Tooltip title="View">
+                <img src={OpenIcon} onClick={() => this.onOpenClick(_id)} alt="open" />
+              </Tooltip>
+              <Tooltip title="Approve">
+                <img src={ApproveIcon} onClick={() => this.onApproveClick(_id)} alt="approve" />
+              </Tooltip>
+              <Tooltip title="Reject">
+                <img
+                  src={CancelIcon}
+                  onClick={() => this.onCancelClick(_id, ticketID)}
+                  alt="cancel"
+                />
+              </Tooltip>
             </div>
           );
 
@@ -125,6 +137,9 @@ class TeamCompoffTable extends PureComponent {
     this.state = {
       pageSelected: 1,
       selectedRowKeys: [],
+      commentModalVisible: false,
+      rejectingId: '',
+      rejectingTicketID: '',
     };
   }
 
@@ -140,12 +155,48 @@ class TeamCompoffTable extends PureComponent {
     this.onOpenClick(_id);
   };
 
-  onApproveClick = () => {
-    alert('Approve');
+  onRefreshTable = (onMovedTab) => {
+    const { onRefreshTable = () => {} } = this.props;
+    onRefreshTable(onMovedTab);
   };
 
-  onCancelClick = () => {
-    alert('Cancel');
+  onApproveClick = async (_id) => {
+    const { dispatch } = this.props;
+    const res = await dispatch({
+      type: 'timeOff/approveCompoffRequest',
+      payload: {
+        _id,
+      },
+    });
+    const { statusCode = 0 } = res;
+    if (statusCode === 200) {
+      this.onRefreshTable('2');
+    }
+  };
+
+  onCancelClick = (_id, ticketID) => {
+    this.toggleCommentModal(true);
+    this.setState({
+      rejectingId: _id,
+      rejectingTicketID: ticketID,
+    });
+  };
+
+  onReject = async (comment) => {
+    const { dispatch } = this.props;
+    const { rejectingId } = this.state;
+    const res = await dispatch({
+      type: 'timeOff/rejectCompoffRequest',
+      payload: {
+        _id: rejectingId,
+        comment,
+      },
+    });
+    const { statusCode = 0 } = res;
+    if (statusCode === 200) {
+      this.toggleCommentModal(false);
+      this.onRefreshTable('3');
+    }
   };
 
   // view request
@@ -223,9 +274,16 @@ class TeamCompoffTable extends PureComponent {
     });
   };
 
+  toggleCommentModal = (value) => {
+    this.setState({
+      commentModalVisible: value,
+    });
+  };
+
   render() {
     const { data = [], loading1, loading2, selectedTab = '' } = this.props;
-    const { selectedRowKeys, pageSelected } = this.state;
+    const { selectedRowKeys, pageSelected, commentModalVisible, rejectingTicketID } = this.state;
+
     const rowSize = 10;
 
     const parsedData = this.processData(data);
@@ -276,6 +334,12 @@ class TeamCompoffTable extends PureComponent {
           dataSource={parsedData}
           scroll={scroll}
           rowKey={(id) => id.ticketID}
+        />
+        <RejectCommentModal
+          visible={commentModalVisible}
+          onClose={() => this.toggleCommentModal(false)}
+          onReject={this.onReject}
+          ticketID={rejectingTicketID}
         />
       </div>
     );
