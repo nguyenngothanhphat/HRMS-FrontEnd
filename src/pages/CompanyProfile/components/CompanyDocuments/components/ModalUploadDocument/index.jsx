b48fcd3b4637c6ff23fbdd6117a69927315202ac
phat nguyen <phat.nguyen@terralogic.com>
2021-01-14 10:12:31 +0700

    import component modal upload documment

--- src/pages/CompanyProfile/components/CompanyDocuments/components/ModalUploadDocument/index.jsx
+++ src/pages/CompanyProfile/components/CompanyDocuments/components/ModalUploadDocument/index.jsx
@@ -1,8 +1,13 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/jsx-curly-newline */
 import React, { Component } from 'react';
-import { Modal, Button, Input, Select } from 'antd';
+import { Modal, Button, Input, Select, Upload, message, Spin } from 'antd';
+import { CloudUploadOutlined, DeleteOutlined } from '@ant-design/icons';
+import { connect } from 'umi';
+import classnames from 'classnames';
 import styles from './index.less';
 
+const { Dragger } = Upload;
 const { Option } = Select;
 
 const listType = [
@@ -10,13 +15,16 @@ const listType = [
   { _id: '45678', name: 'Type 2' },
 ];
 
+@connect(({ loading }) => ({
+  loading: loading.effects['upload/uploadFile'],
+}))
 class ModalUploadDocument extends Component {
   constructor(props) {
     super(props);
     this.state = {
       type: '',
       name: '',
-      // urlImage: '',
+      urlImage: '',
     };
   }
 
@@ -27,7 +35,7 @@ class ModalUploadDocument extends Component {
       this.setState({
         type: '',
         name: '',
-        // urlImage: '',
+        urlImage: '',
       });
     }
     return true;
@@ -39,33 +47,56 @@ class ModalUploadDocument extends Component {
     handleSubmit({ name, type });
   };
 
-  onCancel = () => {
-    const { handleCancel = () => {} } = this.props;
-    this.setState(
-      {
-        type: '',
-        name: '',
-        // urlImage: '',
-      },
-      handleCancel(),
-    );
-  };
-
   onChangeField = (name, value) => {
     this.setState({
       [name]: value,
     });
   };
 
-  render() {
-    const { visible = false, loading = false } = this.props;
-    const {
-      name: valueName,
-      type,
-      // urlImage
-    } = this.state;
-    const checkDisabled = !valueName || !type;
+  beforeUpload = ({ type, size }) => {
+    const checkImg = type === 'image/jpeg' || type === 'application/pdf';
+    if (!checkImg) {
+      message.error('You can only upload JPEG/PDF file!');
+    }
+    const isLt2M = size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!');
+    }
+    return checkImg && isLt2M;
+  };
 
+  handleRemoveFile = () => {
+    this.setState({
+      urlImage: '',
+    });
+  };
+
+  handleUpload = (file) => {
+    const { dispatch } = this.props;
+    const formData = new FormData();
+    formData.append('uri', file);
+    dispatch({
+      type: 'upload/uploadFile',
+      payload: formData,
+    }).then((resp) => {
+      const { statusCode, data = [] } = resp;
+      if (statusCode === 200) {
+        const [first] = data;
+        this.setState({ urlImage: first.url });
+      }
+    });
+  };
+
+  render() {
+    const { visible = false, loadingSubmit = false, handleCancel = () => {}, loading } = this.props;
+    const { name: valueName, type, urlImage } = this.state;
+    const propsUpload = {
+      name: 'file',
+      multiple: false,
+      showUploadList: false,
+    };
+    const checkDisabled = !valueName || !type || !urlImage;
+    console.log('urlImage', urlImage);
     return (
       <Modal
         className={styles.root}
@@ -73,12 +104,12 @@ class ModalUploadDocument extends Component {
         visible={visible}
         title="Upload a new document"
         onOk={this.onOk}
-        onCancel={this.onCancel}
+        onCancel={handleCancel}
         footer={[
           <Button
             key="submit"
             className={styles.btnDone}
-            loading={loading}
+            loading={loadingSubmit}
             onClick={this.onOk}
             disabled={checkDisabled}
           >
@@ -87,7 +118,40 @@ class ModalUploadDocument extends Component {
         ]}
       >
         <div className={styles.content}>
-          <div className={styles.viewUpload}>View Upload</div>
+          <div className={classnames(styles.viewUpload, { [styles.center]: loading })}>
+            {loading ? (
+              <Spin size="large" />
+            ) : (
+              <Dragger
+                {...propsUpload}
+                beforeUpload={this.beforeUpload}
+                action={(file) => this.handleUpload(file)}
+              >
+                <div className={styles.content}>
+                  <div className={styles.viewIconDownload}>
+                    <div className={styles.viewIconDownload__circle}>
+                      <CloudUploadOutlined className={styles.viewIconDownload__circle__icon} />
+                    </div>
+                  </div>
+                  <p className={styles.title}>Drag & drop your file here</p>
+                  <p className={styles.text}>
+                    or <span className={styles.browse}>browse</span> to upload a file
+                  </p>
+                  <p className={styles.textDescription}>
+                    File size should not be more than 2mb. Supported file for view: pdf & jpeg
+                  </p>
+                </div>
+              </Dragger>
+            )}
+          </div>
+          {urlImage && (
+            <div className={styles.viewName}>
+              <p className={styles.viewName__text}>{urlImage.split('/').pop()}</p>
+              <div className={styles.viewName__btnRemove} onClick={this.handleRemoveFile}>
+                <DeleteOutlined className={styles.viewName__btnRemove__icon} />
+              </div>
+            </div>
+          )}
           <div className={styles.content__viewRow}>
             <p>Name of the document</p>
             <div className={styles.content__viewRow__field}>
