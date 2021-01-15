1fa273ab475b215c034c42ba4ae24cb290db60c9
Trung Nguyen <trung.nguyen@terralogic.com>
2021-01-15 09:33:25 +0700

    Update handle submit validation

--- src/components/DirectoryTable/components/ModalTerminate/index.jsx
+++ src/components/DirectoryTable/components/ModalTerminate/index.jsx
@@ -1,83 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { Modal, Input, Form, Button } from 'antd';
 import styles from './index.less';
 
 const { TextArea } = Input;
 
 const ModalTerminate = (props) => {
-    const [form] = Form.useForm();
-    const [isValid, setIsValid] = useState(false)
+  const [form] = Form.useForm();
+  const [isValid, setIsValid] = useState(false);
 
-    const {
-        visible = false,
-        valueReason = '',
-        onChange = () => {},
-        handleSubmit = () => {},
-        handleCandelModal = () => {},
-    } = props;
+  const {
+    visible = false,
+    valueReason = '',
+    onChange = () => {},
+    handleSubmit = () => {},
+    handleCandelModal = () => {},
+  } = props;
 
-    return (
-      <Modal
-        visible={visible}
-        className={styles.terminateModal}
-        title={false}
-        onOk={handleSubmit}
-        onCancel={handleCandelModal}
-        destroyOnClose
-        footer={false}
-      >
-        <div className={styles.contentModal}>
-          <div className={styles.titleModal}>Terminate employee</div>
-          <Form 
-            form={form}
-            onFinish={handleSubmit}
-            onFieldsChange={() =>
-                setIsValid(
-                    form.getFieldsError().some((field) => field.errors.length > 0)
-                )}
+  return (
+    <Modal
+      visible={visible}
+      className={styles.terminateModal}
+      title={false}
+      onOk={handleSubmit}
+      onCancel={handleCandelModal}
+      destroyOnClose
+      footer={false}
+    >
+      <div className={styles.contentModal}>
+        <div className={styles.titleModal}>Terminate employee</div>
+        <Form
+          form={form}
+          onFinish={handleSubmit}
+          initialValues={valueReason}
+          onFieldsChange={() =>
+            setIsValid(form.getFieldsError().some((field) => field.errors.length > 0))
+          }
+        >
+          <Form.Item
+            label="Reason"
+            name="reason"
+            className={styles.formModal}
+            rules={[
+              {
+                pattern: /^[\W\S_]{0,1000}$/,
+                message: 'Only fill up to 1000 characters',
+              },
+              {
+                required: true,
+                message: 'Please input field !',
+              },
+            ]}
           >
-            <Form.Item 
-              label='Reason' 
-              name='reason' 
-              className={styles.formModal}
-              rules={[
-                    {
-                        pattern: /^[\W\S_]{0,1000}$/,
-                        message: 'Only fill up to 1000 characters',
-                    },
-                    {
-                        required: true,
-                        message: 'Please input field !',
-                    },
-                ]}
+            <TextArea
+              className={styles.fieldModal}
+              onChange={onChange}
+              placeholder="Fill in the box..."
+              autoSize={{ minRows: 3, maxRows: 6 }}
+            />
+          </Form.Item>
+          <Form.Item className={styles.flexContent}>
+            <Button
+              className={`${styles.btnGroup} ${styles.btnCancel}`}
+              onClick={handleCandelModal}
             >
-              <TextArea 
-                className={styles.fieldModal}
-                value={valueReason}
-                onChange={onChange}
-                placeholder='Fill in the box...'
-                autoSize={{ minRows: 3, maxRows: 6 }}
-              />
-            </Form.Item>
-            <Form.Item className={styles.flexContent}>
-              <Button
-                className={`${styles.btnGroup} ${styles.btnCancel}`}
-                onClick={handleCandelModal}
-              >
-                Cancel
-              </Button>
-              <Button
-                className={`${styles.btnGroup} ${styles.btnSubmit}`}
-                htmlType='submit'
-                disabled={isValid}
-              >
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
-      </Modal>
-    )
-}
+              Cancel
+            </Button>
+            <Button
+              className={`${styles.btnGroup} ${styles.btnSubmit}`}
+              htmlType="submit"
+              disabled={isValid}
+            >
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </Modal>
+  );
+};
 
-export default ModalTerminate
+export default ModalTerminate;
