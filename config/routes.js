f2c09ae78ce371eb1e0c8b6a63d1ce3c848c51a3
Lewis Nguyen <lewis.nguyen@terralogic.com>
2021-01-19 15:01:58 +0700

    change proxy

--- config/routes.js
+++ config/routes.js
@@ -125,7 +125,7 @@ const routes = [
           {
             path: '/dashboard',
             name: 'dashboard',
-            icon: '/assets/images/home.svg',
+            icon: '/assets/images/menuIcons/dashboard.svg',
             component: './Dashboard',
           },
           {
@@ -236,7 +236,7 @@ const routes = [
           {
             path: '/directory',
             name: 'directory',
-            icon: '/assets/images/directory.svg',
+            icon: '/assets/images/menuIcons/directory.svg',
             component: './Directory',
             authority: ['P_DIRECTORY_VIEW'],
           },
@@ -317,14 +317,14 @@ const routes = [
           {
             path: '/employee-onboarding',
             name: 'employeeOnboarding',
-            icon: '/assets/images/onboarding.svg',
+            icon: '/assets/images/menuIcons/onboarding.svg',
             component: './EmployeeOnboarding',
             authority: ['P_ONBOARDING_VIEW'],
           },
           {
             path: '/time-off',
             name: 'timeOff',
-            icon: '/assets/images/timeOff.svg',
+            icon: '/assets/images/menuIcons/timeoff.svg',
             component: './TimeOff',
             // authority: ['P_TIMEOFF_VIEW'],
             authority: [
@@ -373,7 +373,7 @@ const routes = [
           {
             path: '/offboarding',
             name: 'OffBoarding',
-            icon: '/assets/images/iconOffboarding.svg',
+            icon: '/assets/images/menuIcons/offboarding.svg',
             component: './OffBoarding',
             authority: ['employee', 'leader', 'manager', 'hr', 'hr-manager'],
           },
