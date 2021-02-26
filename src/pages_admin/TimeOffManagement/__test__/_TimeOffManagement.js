fb3ece9044c71cc551e4dfd26a90ed012bb6a707
tuan.luong <tuan.luong@terralogic.com>
2021-02-26 17:07:25 +0700

    update test

---
+++ src/pages_admin/TimeOffManagement/__test__/_TimeOffManagement.js
@@ -0,0 +1,10 @@
+const puppeteer = require('puppeteer-core');
+
+(async () => {
+    const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:8000/time-off-management');
+  console.log(await page.content())
+  // puppeteer.
+  await browser.close();
+})()
