144959c3bcca0588b09787daae46d53489453d2c
Trung Nguyen <nguyenchitrung1310@gmail.com>
2021-03-24 16:26:03 +0700

    Update layout

--- src/pages/CompanyProfile/components/Administrator/index.jsx
+++ src/pages/CompanyProfile/components/Administrator/index.jsx
@@ -1,4 +1,6 @@
 import React, { PureComponent } from 'react';
+import { Row, Col } from 'antd';
+
 import styles from './index.less';
 
 class Administrator extends PureComponent {
@@ -14,7 +16,19 @@ class Administrator extends PureComponent {
           <div className={styles.header__title}>Primary administrator</div>
           <div className={styles.header__action}>Change</div>
         </div>
-        <div className={styles.primaryList}>hello</div>
+        <div className={styles.primaryList}>
+          <Row gutter={[24, 12]}>
+            <Col span={8}>
+              <div className={styles.primaryList__left} />
+            </Col>
+            <Col span={8}>
+              <div className={styles.primaryList__center} />
+            </Col>
+            <Col span={8}>
+              <div className={styles.primaryList__right} />
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
