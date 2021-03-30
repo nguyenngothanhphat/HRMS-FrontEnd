aab524e19000a482801a499f403ea3373d0f378d
Trung Nguyen <nguyenchitrung1310@gmail.com>
2021-03-30 14:42:32 +0700

    Update position content

--- src/pages/AdminApp/components/Administrator/index.jsx
+++ src/pages/AdminApp/components/Administrator/index.jsx
@@ -9,24 +9,24 @@ import AddAdmin from './components/AddAdmin';
 import styles from './index.less';
 import EditAdmin from './components/EditAdmin';
 
-const listAdministrator = [
-  {
-    permissionModule: [
-      'M_USER_MANAGEMENT_LIST_USER_B_DEACTIVE_USER_VIEW',
-      'M_ONBOARDING_T_ONBOARDING_OVERVIEW_VIEW',
-      'M_USER_MANAGEMENT_LIST_USER_ALL',
-    ],
-    firstName: 'Jenny',
-    email: 'jenny@terralogic.com',
-    position: 'Jenny’s permission apply to everyone in the company',
-  },
-  {
-    permissionModule: ['M_DIRECTORY_T_DIRECTORY_T_ACTIVE_EMPLOYEE_VIEW'],
-    firstName: 'Renil Komitla',
-    email: 'renil@terralogic.com',
-    position: 'Renil’s permission apply to everyone in the company',
-  },
-];
+// const listAdministrator = [
+//   {
+//     permissionModule: [
+//       'M_USER_MANAGEMENT_LIST_USER_B_DEACTIVE_USER_VIEW',
+//       'M_ONBOARDING_T_ONBOARDING_OVERVIEW_VIEW',
+//       'M_USER_MANAGEMENT_LIST_USER_ALL',
+//     ],
+//     firstName: 'Jenny',
+//     email: 'jenny@terralogic.com',
+//     position: 'Jenny’s permission apply to everyone in the company',
+//   },
+//   {
+//     permissionModule: ['M_DIRECTORY_T_DIRECTORY_T_ACTIVE_EMPLOYEE_VIEW'],
+//     firstName: 'Renil Komitla',
+//     email: 'renil@terralogic.com',
+//     position: 'Renil’s permission apply to everyone in the company',
+//   },
+// ];
 
 @connect(({ adminApp: { permissionList = [], listAdmin = [] }, loading }) => ({
   permissionList,
@@ -91,30 +91,30 @@ class Adminstrator extends Component {
     });
   };
 
-  listAdmin = () => {
-    const { permissionList = [], listAdmin = [] } = this.props;
-    return listAdmin.map((ad) => {
-      const { permissionAdmin = [], usermap: { email = '', firstName = '' } = {} } = ad;
-      const listModuleName = [];
-      permissionList.forEach((per) => {
-        const { _id = '', module = '' } = per;
-        permissionAdmin.forEach((mol) => {
-          if (mol === _id)
-            listModuleName.push({
-              id: _id,
-              role: module,
-            });
-        });
-      });
+  // dataListAdmin = () => {
+  //   const { permissionList = [], listAdmin = [] } = this.props;
+  //   return listAdmin.map((ad) => {
+  //     const { permissionAdmin = [], usermap: { email = '', firstName = '' } = {} } = ad;
+  //     const listModuleName = [];
+  //     permissionList.forEach((per) => {
+  //       const { _id = '', module = '' } = per;
+  //       permissionAdmin.forEach((mol) => {
+  //         if (mol === _id)
+  //           listModuleName.push({
+  //             id: _id,
+  //             role: module,
+  //           });
+  //       });
+  //     });
 
-      return {
-        listRole: listModuleName,
-        employeeName: firstName,
-        email,
-        position: 'Renil’s permission apply to everyone in the company',
-      };
-    });
-  };
+  //     return {
+  //       listRole: listModuleName,
+  //       employeeName: firstName,
+  //       email,
+  //       position: `${firstName}’s permission apply to everyone in the company`,
+  //     };
+  //   });
+  // };
 
   render() {
     const { isAddAdmin, isEditAdmin, dataAdmin = {} } = this.state;
