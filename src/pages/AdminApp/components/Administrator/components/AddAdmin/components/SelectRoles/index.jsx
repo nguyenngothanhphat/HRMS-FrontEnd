25f88c34079e1967c8e7c56597e556330247b127
Trung Nguyen <nguyenchitrung1310@gmail.com>
2021-04-01 16:56:54 +0700

    Filter permissions root ID

--- src/pages/AdminApp/components/Administrator/components/AddAdmin/components/SelectRoles/index.jsx
+++ src/pages/AdminApp/components/Administrator/components/AddAdmin/components/SelectRoles/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-globals */
 import React, { PureComponent } from 'react';
 import { Button, Tree } from 'antd';
 // import { PlusSquareOutlined, MinusSquareOutlined } from '@ant-design/icons';
@@ -29,8 +30,32 @@ class SelectRoles extends PureComponent {
     });
   };
 
+  filterID = (value) => {
+    const idArray = [
+      'M_USER_MANAGEMENT',
+      'M_DIRECTORY',
+      'M_ONBOARDING',
+      'M_TIMEOFF',
+      'M_OFFBOARDING',
+      'M_PROJECT_MANAGEMENT',
+      'M_DOCUMENT_MANAGEMENT',
+    ];
+
+    let data = idArray.map((item) => {
+      if (value.includes(item)) {
+        return item;
+      }
+      return 0;
+    });
+
+    data = data.filter((item) => item !== 0);
+    return data;
+  };
+
   renderList = () => {
     const { permissionList = [] } = this.props;
+    const root = [];
+
     let formatList = permissionList.map((per) => per?.module);
     formatList = formatList.filter(
       (value) => value !== undefined && value !== '' && value !== null,
@@ -50,15 +75,49 @@ class SelectRoles extends PureComponent {
       });
       result = result.filter((val) => val !== 0);
 
+      // remove a result that its Title contains keyword 'root view'
+      let filterResult = result.map((res) => {
+        const { title = '' } = res;
+        if (!title.includes('root view')) {
+          return res;
+        }
+        root.push(res);
+        return 0;
+      });
+
+      filterResult = filterResult.filter((val) => val !== 0);
+
       return {
         key: index,
         title: moduleName,
-        children: result,
+        children: filterResult,
       };
     });
 
     const onCheck = (checkedKeys) => {
-      this.setList(checkedKeys);
+      // Filter value IDs that include a part of rootID string in function filterID()
+      let arrayValueID = [];
+      checkedKeys.forEach((item) => {
+        if (this.filterID(item)) {
+          arrayValueID.push(...this.filterID(item));
+        }
+      });
+      arrayValueID = [...new Set(arrayValueID)];
+
+      // then, return root id from root list
+      const filterRootID = [];
+      arrayValueID.map((id) => {
+        root.forEach((r) => {
+          if (r.key.includes(id)) {
+            filterRootID.push(r.key);
+          }
+        });
+        return 0;
+      });
+
+      const formatPermission = checkedKeys.filter((item) => isNaN(item));
+      const listPermission = [...formatPermission, ...filterRootID];
+      this.setList(listPermission);
     };
 
     return (
