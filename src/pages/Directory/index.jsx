93025246055f216e196f001c2cf55dcd25efe267
Trung Nguyen <nguyenchitrung1310@gmail.com>
2021-04-22 08:40:20 +0700

    Update props DirectoryComponent

--- src/pages/Directory/index.jsx
+++ src/pages/Directory/index.jsx
@@ -127,7 +127,7 @@ class Directory extends PureComponent {
             // tabBarExtraContent={checkRoleEmployee ? '' : this.operations()}
           >
             <TabPane tab={formatMessage({ id: 'pages.directory.directoryTab' })} key="1">
-              <DirectoryComponent />
+              <DirectoryComponent checkRoleEmployee={checkRoleEmployee} />
             </TabPane>
             {/* <TabPane tab={formatMessage({ id: 'pages.directory.organisationChartTab' })} key="2">
               <OrganChart />
