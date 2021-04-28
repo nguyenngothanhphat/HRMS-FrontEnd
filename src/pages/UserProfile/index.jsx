fb52f9df4e3ebfd7724e15a8c733e09a709342f2
Trung Nguyen <nguyenchitrung1310@gmail.com>
2021-04-28 15:25:43 +0700

    Create and update new UI user profile page

--- src/pages/UserProfile/index.jsx
+++ src/pages/UserProfile/index.jsx
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { Affix, Skeleton } from 'antd';
+
+import { PageContainer } from '@/layouts/layout/src';
+import { getCurrentTenant, getCurrentCompany } from '@/utils/authority';
+
+import styles from './index.less';
 
 class UserProfile extends Component {
   constructor(props) {
@@ -7,7 +13,27 @@ class UserProfile extends Component {
   }
 
   render() {
-    return <div>User profile</div>;
+    const tenant = getCurrentTenant();
+    const company = getCurrentCompany();
+
+    return (
+      <PageContainer>
+        <div className={styles.containerUserProfile}>
+          <Affix offsetTop={42}>
+            <div className={styles.titlePage}>
+              <p className={styles.titlePage__text}>User Profile</p>
+            </div>
+          </Affix>
+          {tenant && company ? (
+            <div>User profile</div>
+          ) : (
+            <div style={{ padding: '24px' }}>
+              <Skeleton />
+            </div>
+          )}
+        </div>
+      </PageContainer>
+    );
   }
 }
 
