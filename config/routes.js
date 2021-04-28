c5b59fdb9c9ebae2410694877a7eafc6faf90b20
Trung Nguyen <nguyenchitrung1310@gmail.com>
2021-04-28 15:19:45 +0700

    Update router name

--- config/routes.js
+++ config/routes.js
@@ -223,7 +223,7 @@ const routes = [
           },
           {
             path: '/user-profile/:reId',
-            name: 'userProfile',
+            name: 'User Profile',
             component: './UserProfile',
             hideInMenu: true,
             // authority: ['admin-sa'],
