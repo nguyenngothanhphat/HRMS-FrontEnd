3dc50b265ab7dd02cb74e9450404704580e354ce
Nguyen Chi Trung <nguyenchitrung1310@gmail.com>
2021-06-02 11:07:05 +0700

    Update Edit, Approval button and Set 1 on 1 flow

--- src/pages/OffBoarding/HrOffboarding/component/HrRequestTable/Ticket/components/RequestChangeLWD/index.jsx
+++ src/pages/OffBoarding/HrOffboarding/component/HrRequestTable/Ticket/components/RequestChangeLWD/index.jsx
@@ -87,7 +87,7 @@ class RequestChangeLWD extends Component {
   };
 
   openEdit = (nodeStep) => {
-    if (nodeStep === 4) {
+    if (nodeStep === 3 || nodeStep === 4) {
       this.setState({ isEdit: true });
     }
   };
@@ -139,7 +139,7 @@ class RequestChangeLWD extends Component {
               <div
                 className={styles.editBtn}
                 onClick={() => this.openEdit(nodeStep)}
-                style={nodeStep === 4 ? null : { opacity: 0.5 }}
+                style={nodeStep > 4 || nodeStep < 3 ? { opacity: 0.5 } : null}
               >
                 <span>Edit</span>
               </div>
@@ -238,7 +238,7 @@ class RequestChangeLWD extends Component {
                 </>
               ) : (
                 <Button
-                  disabled={nodeStep !== 4}
+                  disabled={nodeStep > 4 || nodeStep < 3}
                   className={styles.btnSubmit}
                   onClick={() => this.handleRequestChangeLWD('ACCEPTED')}
                 >
