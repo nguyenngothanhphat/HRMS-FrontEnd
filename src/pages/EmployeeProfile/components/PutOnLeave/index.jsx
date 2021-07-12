b2502e548f0b2af3ef57499d3108c0bb4a7d66f1
Nguyen Chi Trung <trung.nguyen@terralogic.com>
2021-07-12 18:20:25 +0700

    Add form fields LWP

--- src/pages/EmployeeProfile/components/PutOnLeave/index.jsx
+++ src/pages/EmployeeProfile/components/PutOnLeave/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-// import edit from '@/asset/edit.svg';
+import { Form, DatePicker, Row, Col } from 'antd';
+import warning from '@/assets/warning_filled.svg';
 import path from '@/assets/path.svg';
 
 import styles from './index.less';
@@ -26,7 +27,74 @@ class PutOnLeave extends Component {
               <span className={styles.editBtn}>Cancel & Return</span>
             </div>
           </div>
-          <div className={styles.viewBottom}>PutOnLeave</div>
+          <div className={styles.viewBottom}>
+            <div className={styles.notification}>
+              <img alt="warning" src={warning} />
+              <div className={styles.notification__text}>
+                This will put Aditya Venkatesh on leave for the days selected without pay, a
+                Notification for the update will be sent to the employee as well.
+              </div>
+            </div>
+            <div className={styles.lwpForm}>
+              <Form className={styles.lwpForm__form}>
+                <Row className={styles.lwpForm__form__row} gutter={[24, 0]}>
+                  <Col className={styles.lwpForm__form__col} xs={24} sm={24} md={6} lg={6} xl={6}>
+                    <div className={styles.lwpText}>Leave From</div>
+                  </Col>
+                  <Col
+                    className={styles.lwpForm__form__col}
+                    xs={24}
+                    sm={24}
+                    md={18}
+                    lg={18}
+                    xl={18}
+                  >
+                    <Form.Item
+                      name="leaveFrom"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please select Leave from date',
+                        },
+                      ]}
+                    >
+                      <DatePicker
+                        className={styles}
+                        placeholder="DD-MM-YYYY"
+                        picker="date"
+                        format="DD.MM.YYYY"
+                        // onChange={(value) => _handleSelect(value, candidateField[1].title)}
+                      />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Row className={styles.lwpForm__form__row} gutter={[24, 0]}>
+                  <Col xs={24} sm={24} md={6} lg={6} xl={6}>
+                    <div className={styles.lwpText}>Leave To</div>
+                  </Col>
+                  <Col xs={24} sm={24} md={18} lg={18} xl={18}>
+                    <Form.Item
+                      name="leaveTo"
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please select Leave to date',
+                        },
+                      ]}
+                    >
+                      <DatePicker
+                        className={styles}
+                        placeholder="DD-MM-YYYY"
+                        picker="date"
+                        format="DD.MM.YYYY"
+                        // onChange={(value) => _handleSelect(value, candidateField[1].title)}
+                      />
+                    </Form.Item>
+                  </Col>
+                </Row>
+              </Form>
+            </div>
+          </div>
         </div>
       </div>
     );
