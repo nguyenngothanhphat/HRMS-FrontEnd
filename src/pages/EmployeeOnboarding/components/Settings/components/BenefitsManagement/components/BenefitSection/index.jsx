685331aeccd2b5c3081a04b865a3c266cc40586c
Nguyen Chi Trung <trung.nguyen@terralogic.com>
2021-07-30 22:07:33 +0700

    Add new field select location

--- src/pages/EmployeeOnboarding/components/Settings/components/BenefitsManagement/components/BenefitSection/index.jsx
+++ src/pages/EmployeeOnboarding/components/Settings/components/BenefitsManagement/components/BenefitSection/index.jsx
@@ -1,15 +1,83 @@
 import React, { Component } from 'react';
+import { Button, Select, Spin } from 'antd';
+import { connect } from 'umi';
 
 import styles from './index.less';
 
+const { Option } = Select;
+@connect(({ country: { listCountry = [] } = {}, loading }) => ({
+  listCountry,
+  loadingFetchCountry: loading.effects['country/fetchListCountry'],
+}))
 class BenefitSection extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
+  componentDidMount = () => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'country/fetchListCountry',
+    });
+  };
+
   render() {
-    return <div className={styles.benefitSection}>Benefit</div>;
+    const { listCountry = [], loadingFetchCountry } = this.props;
+    if (loadingFetchCountry)
+      return (
+        <div className={styles.loadingSpin}>
+          <Spin />
+        </div>
+      );
+    return (
+      <div className={styles.benefitSection}>
+        <div className={styles.benefitRoot}>
+          <div className={styles.benefitRoot__header}>
+            <div className={styles.benefitRoot__header__text}>
+              <div className={styles.title}>Benefits</div>
+              <div className={styles.subTitle}>
+                The list of benefits the candidate is eligible for is populated below.
+              </div>
+            </div>
+            <Button>Add a Benefit</Button>
+          </div>
+
+          <div className={styles.benefitRoot__form}>
+            <div className={styles.benefitRoot__form__text}>Choose Location</div>
+            <div className={styles.benefitRoot__form__selectCountry}>
+              <Select
+                showSearch
+                showArrow
+                allowClear
+                loading={loadingFetchCountry}
+                placeholder="Select location"
+                onChange={this.onChangeSelect}
+                filterOption={(input, option) => {
+                  return option.props.children[1].toLowerCase().indexOf(input.toLowerCase()) >= 0;
+                }}
+              >
+                {listCountry.map((item) => (
+                  <Option value={item._id}>
+                    <img
+                      style={{
+                        width: '24px',
+                        height: '24px',
+                        borderRadius: '50%',
+                        marginRight: '8px',
+                      }}
+                      src={item.flag}
+                      alt="flag"
+                    />
+                    {item.name}
+                  </Option>
+                ))}
+              </Select>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
