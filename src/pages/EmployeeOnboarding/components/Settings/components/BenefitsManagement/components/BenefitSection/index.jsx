f15776854a70281d9c0764f1ccb744d53997bd16
Nguyen Chi Trung <trung.nguyen@terralogic.com>
2021-08-02 09:20:43 +0700

    Add modal add benefit

--- src/pages/EmployeeOnboarding/components/Settings/components/BenefitsManagement/components/BenefitSection/index.jsx
+++ src/pages/EmployeeOnboarding/components/Settings/components/BenefitsManagement/components/BenefitSection/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, Spin } from 'antd';
 import { connect } from 'umi';
 import BenefitSection from './components/BenefitSection';
+import ModalAddBenefit from './components/ModaAddBenefit';
 
 import styles from './index.less';
 
@@ -11,7 +12,9 @@ import styles from './index.less';
 class BenefitPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      visible: false,
+    };
   }
 
   // componentDidMount = () => {
@@ -21,8 +24,18 @@ class BenefitPage extends Component {
   //   });
   // };
 
+  openModal = () => {
+    this.setState({ visible: true });
+  };
+
+  closeModal = () => {
+    this.setState({ visible: false });
+  };
+
   render() {
     const { loadingFetchCountry } = this.props;
+    const { visible } = this.state;
+
     if (loadingFetchCountry)
       return (
         <div className={styles.loadingSpin}>
@@ -39,13 +52,14 @@ class BenefitPage extends Component {
                 The list of benefits the candidate is eligible for is populated below.
               </div>
             </div>
-            <Button>Add a Benefit</Button>
+            <Button onClick={this.openModal}>Add a Benefit</Button>
           </div>
 
           <div className={styles.benefitRoot__bottom}>
             <BenefitSection />
           </div>
         </div>
+        <ModalAddBenefit visible={visible} handleCandelModal={this.closeModal} />
       </div>
     );
   }
