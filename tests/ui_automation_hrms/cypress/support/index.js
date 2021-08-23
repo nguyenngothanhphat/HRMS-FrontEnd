522d77749c57da79f97344cad62b60889e56331a
huu.nguyen <huu.nguyen@terralogic.com>
2021-08-23 17:54:32 +0700

    change path cypress and add auto test onboarding

--- tests/ui_automation_hrms/cypress/support/index.js
+++ tests/ui_automation_hrms/cypress/support/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // ***********************************************************
 // This example support/index.js is processed and
 // loaded automatically before your test files.
@@ -19,28 +20,37 @@ import './commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-
-
-
 // code to ignore resize observer loop errors
 const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/;
+// eslint-disable-next-line consistent-return
 Cypress.on('uncaught:exception', (err) => {
-    /* returning false here prevents Cypress from failing the test */
-    if (resizeObserverLoopErrRe.test(err.message)) {
-        return false;
-    }
+  /* returning false here prevents Cypress from failing the test */
+  if (resizeObserverLoopErrRe.test(err.message)) {
+    return false;
+  }
 });
 
-
-Cypress.Commands.add('loginAsSomeone',(email, password) => {
-    cy.get('#basic_email.ant-input', {timeout:8000}).type(email);
-    cy.get('#basic_password.ant-input', {timeout:8000}).type(password);
-    cy.get('button[type="submit"]', {timeout:8000}).click();
+Cypress.Commands.add('loginAsSomeone', (email, password) => {
+  cy.get('#basic_userEmail.ant-input', { timeout: 8000 }).type(email);
+  cy.get('#basic_password.ant-input', { timeout: 8000 }).type(password);
+  cy.get('button[type="submit"]', { timeout: 8000 }).click();
 });
 
-Cypress.Commands.add('logout',() => {
-    cy.get(".ant-dropdown-trigger", {timeout:8000}).then((resp)=>{
-        cy.get(resp[1]).trigger('mousemove').click();
-        cy.contains("Logout").click();
-    });
+Cypress.Commands.add('logout', () => {
+  cy.get('.ant-dropdown-trigger', { timeout: 8000 }).then((resp) => {
+    cy.get(resp[1]).trigger('mousemove').click();
+    cy.contains('Logout').click();
+  });
+});
+
+Cypress.Commands.add('iframe', { prevSubject: 'element' }, ($iframe, callback = () => {}) => {
+  // For more info on targeting inside iframes refer to this GitHub issue:
+  // https://github.com/cypress-io/cypress/issues/136
+  cy.log('Getting iframe body');
+
+  return cy
+    .wrap($iframe)
+    .should((iframe) => expect(iframe.contents().find('body')).to.exist)
+    .then((iframe) => cy.wrap(iframe.contents().find('body')))
+    .within({}, callback);
 });
