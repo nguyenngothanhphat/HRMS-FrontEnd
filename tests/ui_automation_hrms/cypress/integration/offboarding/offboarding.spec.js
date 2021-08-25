39f4b7b3e2c4917ded31a34d50452f7dafdcb02c
huu.nguyen <huu.nguyen@terralogic.com>
2021-08-25 17:57:58 +0700

    fix offboarding

--- tests/ui_automation_hrms/cypress/integration/offboarding/offboarding.spec.js
+++ tests/ui_automation_hrms/cypress/integration/offboarding/offboarding.spec.js
@@ -1,38 +1,44 @@
 /* eslint-disable no-undef */
 /* eslint-disable camelcase */
 /// <reference types="cypress" />
-const host_url = Cypress.env('HOST_URL');
-const hr_email = Cypress.env('hr_email');
-// const hrManager_email = Cypress.env('hrManager_email');
-const hrManager_email = 'linh.kai@mailinator.com';
-const employee_email = Cypress.env('employee_email');
+
+const hrManager_email = Cypress.env('hrManager_email');
 const manager_email = Cypress.env('manager_email');
 
 const password = Cypress.env('password');
-// info employee
+// create date metting 1-on-1
+const date = new Date();
+date.setDate(date.getDate() + 1);
+date.setHours(12, 0, 0);
+const mettingDate = Cypress.moment(date).format('YYYY-MM-DD');
 
 describe('Offboaring automation', () => {
   before(() => {
-    cy.visit(host_url);
+    cy.clock(date.getTime());
+    cy.visit('/');
   });
 
   it('Employee submit resignation', () => {
-    cy.loginAsSomeone(employee_email, password);
-    cy.contains('Please select a company profile to proceed', {
-      timeout: 10000,
+    cy.readFile('cypress/fixtures/directory.json').then((item) => {
+      cy.loginAsSomeone(item.email, item.password);
     });
-    cy.contains('TERRALOGIC')
-      .parentsUntil('.companiesContainer___3Yqdu')
-      .contains('Get Started')
-      .click();
+    //
+    // cy.contains('Please select a company profile to proceed', {
+    //   timeout: 10000,
+    // });
+    // cy.contains('TERRALOGIC')
+    //   .parentsUntil('.companiesContainer___3Yqdu')
+    //   .contains('Get Started')
+    //   .click();
     cy.wait(4000);
+
     cy.get('.account___1r_Ku')
       .trigger('mousemove')
       .click({ force: true })
       .then(() => {
         cy.contains('View profile').click({ force: true });
       });
-    cy.wait(3000);
+    cy.wait(5000);
     cy.get('.viewBtnAction___3cOs4 > .ant-dropdown-trigger')
       .should('have.text', 'Actions ')
       .click();
@@ -41,12 +47,12 @@ describe('Offboaring automation', () => {
       .then(() => {
         cy.contains('Offboarding').click({ force: true });
       });
-    cy.wait(3000);
+    cy.wait(5000);
     cy.get('.stepAction___3W-HR > .ant-btn')
       .should('have.text', 'Initiate resignation request')
       .click();
     cy.wait(3000);
-    cy.get('.titleBody___35uUa > .ant-input').type('I want off');
+    cy.get('.titleBody___35uUa > .ant-input').type('Cypress auto testing offboarding');
     cy.get('.buttonSubmit___Qqr32').should('be.enabled').click();
     cy.wait(5000);
     cy.get('.rowAction__view___wge0z').click({ force: true });
@@ -67,46 +73,74 @@ describe('Offboaring automation', () => {
     cy.logout();
   });
 
-  // Manager Accept
-  it('Manager Accept', () => {
+  // Manager set metting 1-on-1
+  it('Manager set metting 1-on-1', () => {
     cy.loginAsSomeone(manager_email, password);
-    cy.contains('Please select a company profile to proceed', {
-      timeout: 10000,
-    });
-    cy.contains('TERRALOGIC')
-      .parentsUntil('.companiesContainer___3Yqdu')
-      .contains('Get Started')
-      .click();
-    cy.wait(4000);
+    // cy.contains('Please select a company profile to proceed', {
+    //   timeout: 10000,
+    // });
+    // cy.contains('TERRALOGIC')
+    //   .parentsUntil('.companiesContainer___3Yqdu')
+    //   .contains('Get Started')
+    //   .click();
+    // cy.wait(4000);
+
     cy.contains('Offboarding').click({ force: true });
+    cy.wait(3000);
+    cy.get(
+      '.tabTable___2D0zx > .ant-tabs > .ant-tabs-nav > .ant-tabs-nav-wrap > .ant-tabs-nav-list > :nth-child(2)',
+    ).click();
     cy.wait(3000);
     cy.readFile('cypress/fixtures/offboarding_ticket.json').then((item) => {
       cy.contains(item.ticketId).click();
     });
+
     cy.wait(3000);
     cy.get('.content___3VE8G > .ant-btn').should('have.text', 'Schedule a 1-on-1').click();
     cy.wait(1000);
-    cy.get('.ant-picker-input > input').type('2021-08-24{enter}', { force: true });
+    cy.get('.ant-picker-input > input').type(`${mettingDate}{enter}`, { force: true });
     cy.get('#rc_select_0')
       .click()
       .then(() => {
         cy.get('[title="10:00(am) - 11:00(am)"]').click({ force: true });
       });
-    cy.get(':nth-child(4) > .ant-btn').should('be.enabled').click();
+
+    cy.wait(1000);
+    cy.get(':nth-child(4) > .ant-btn').should('have.text', 'Submit').should('be.enabled').click();
     cy.wait(3000);
-    cy.pause();
-    cy.get('.text___2P8vW').click();
+    cy.logout();
+  });
+
+  // Manager Accept
+  it.only('Manager Accept', () => {
+    cy.loginAsSomeone(manager_email, password);
+
+    cy.contains('Offboarding').click({ force: true });
+    cy.wait(3000);
+
+    cy.get(
+      '.tabTable___2D0zx > .ant-tabs > .ant-tabs-nav > .ant-tabs-nav-wrap > .ant-tabs-nav-list > :nth-child(2)',
+    ).click();
+    cy.wait(3000);
+    cy.readFile('cypress/fixtures/offboarding_ticket.json').then((item) => {
+      cy.contains(item.ticketId).click();
+    });
+    cy.clock(date.getTime());
+
+    cy.wait(3000);
+    cy.get('.text___2P8vW').should('have.text', 'Enter Closing Comments').click();
     cy.get('.textArea___1dVk7 > .ant-input-affix-wrapper > .ant-input').type('Accepted');
     cy.get('.buttonArea___3LmGL > .ant-btn').should('be.enabled').click();
+    cy.clock().invoke('restore');
     cy.wait(2000);
-    cy.get('.ant-btn-primary').click();
+    cy.get('.ant-btn-primary').should('have.text', 'Accept').click();
+    cy.wait(3000);
     cy.get('.modal__content___2yg-F > .ant-btn', { timeout: 10000 })
       .should('have.text', 'OK')
       .click();
     cy.wait(2000);
     cy.logout();
   });
-
   // hr verify
   it('HR verify', () => {
     cy.loginAsSomeone(hrManager_email, password);
@@ -177,15 +211,18 @@ describe('Offboaring automation', () => {
 
   // employee submit exit package
   it('Employee Submit exit package', () => {
-    cy.loginAsSomeone(employee_email, password);
-    cy.contains('Please select a company profile to proceed', {
-      timeout: 10000,
+    cy.readFile('cypress/fixtures/directory.json').then((item) => {
+      cy.loginAsSomeone(item.email, item.password);
     });
-    cy.contains('TERRALOGIC')
-      .parentsUntil('.companiesContainer___3Yqdu')
-      .contains('Get Started')
-      .click();
-    cy.wait(4000);
+
+    // cy.contains('Please select a company profile to proceed', {
+    //   timeout: 10000,
+    // });
+    // cy.contains('TERRALOGIC')
+    //   .parentsUntil('.companiesContainer___3Yqdu')
+    //   .contains('Get Started')
+    //   .click();
+    // cy.wait(4000);
     cy.get('.account___1r_Ku')
       .trigger('mousemove')
       .click({ force: true })
@@ -282,6 +319,6 @@ describe('Offboaring automation', () => {
       .should('have.text', 'Close employee record')
       .should('be.enabled')
       .click();
-    cy.wait(3000)
+    cy.wait(3000);
   });
 });
