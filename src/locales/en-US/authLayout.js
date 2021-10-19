12650ecf5618a982b76d4f76e8037829b4eb9c6c
Lewis Nguyen <lewis.nguyen@terralogic.com>
2021-10-19 10:43:36 +0700

    change request from umi to axios

--- src/locales/en-US/authLayout.js
+++ src/locales/en-US/authLayout.js
@@ -5,5 +5,5 @@ export default {
   'layout.authLayout.contentLeft.text1.1': 'Spending too much time on HR, not your business?',
   'layout.authLayout.contentLeft.text1.2': 'We can fix that.',
   'layout.authLayout.contentLeft.text2':
-    'Streamline onboarding, benefits, payroll, PTO, and more with our simple, intuitive platform...',
+    'Streamline onboarding, benefits, payroll, PTO, and more with our simple, intuitive platform',
 };
