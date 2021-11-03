801f3f6757d5acf66ac3b252934c06ea2f182977
thiensinh127 <thiensinh127@gmail.com>
2021-11-03 11:25:13 +0700

    update

--- src/pages/TicketManagement/EmployeeTickets/index.jsx
+++ src/pages/TicketManagement/EmployeeTickets/index.jsx
@@ -5,38 +5,39 @@ import { PageContainer } from '@/layouts/layout/src';
 import TicketQueue from './components/TicketQueue';
 import MyTickets from './components/MyTickets';
 import styles from './index.less';
+
 const { TabPane } = Tabs;
 
 class EmployeeTicket extends Component {
+  componentDidMount() {
+    const { tabName = '' } = this.props;
+    if (!tabName) {
+      history.replace(`/ticket-management/allticket`);
+    }
+  }
 
-    componentDidMount() {
-        const { tabName = '' } = this.props;
-        if (!tabName) {
-            history.replace(`/ticket-management/allticket`);
-        }
-    }
-    render() {
-        const { tabName = '' } = this.props;
-        return (
-            <div className={styles.TicketManagement}>
-                <PageContainer>
-                    <Tabs
-                        activeKey={tabName || 'ticketqueue'}
-                        onChange={(key) => {
-                            history.push(`/ticket-management/${key}`);
-                        }}
-                    >
-                        <TabPane tab="TicketQueue" key="ticketqueue">
-                            <TicketQueue />
-                        </TabPane>
-                        <TabPane tab="MyTickets" key="mytickets">
-                            <MyTickets />
-                        </TabPane>
-                    </Tabs>
-                </PageContainer>
-            </div>
-        )
-    }
+  render() {
+    const { tabName = '' } = this.props;
+    return (
+      <div className={styles.TicketManagement}>
+        <PageContainer>
+          <Tabs
+            activeKey={tabName || 'ticketqueue'}
+            onChange={(key) => {
+              history.push(`/ticket-management/${key}`);
+            }}
+          >
+            <TabPane tab="TicketQueue" key="ticketqueue">
+              <TicketQueue />
+            </TabPane>
+            <TabPane tab="MyTickets" key="mytickets">
+              <MyTickets />
+            </TabPane>
+          </Tabs>
+        </PageContainer>
+      </div>
+    );
+  }
 }
 
-export default EmployeeTicket
+export default EmployeeTicket;
