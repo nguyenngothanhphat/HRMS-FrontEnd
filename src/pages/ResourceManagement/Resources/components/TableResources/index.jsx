7e0bd9af739836271a467ce0f04f43d9bfabe07b
nghia.vo <nghia.vo@terralogic.com>
2021-11-10 10:24:07 +0700

    showAssignToProject
    	modified:   src/assets/resource-action-add.svg
    	new file:   src/assets/resource-action-add1.svg
    	new file:   src/assets/resource-management-datepicker.svg
    	new file:   src/assets/resource-management-edit1.svg
    	modified:   src/pages/ResourceManagement/Resources/components/TableResources/index.jsx
    	modified:   src/pages/ResourceManagement/Resources/components/TableResources/index.less

--- src/pages/ResourceManagement/Resources/components/TableResources/index.jsx
+++ src/pages/ResourceManagement/Resources/components/TableResources/index.jsx
@@ -1,16 +1,13 @@
-import React, { PureComponent } from 'react';
-import {
-  Button,
-  Table,
-  // , Dropdown, Menu, Input
-} from 'antd';
+
+import React, { PureComponent, useState } from 'react';
+import { Table, Row, Col, Button, Modal, Form, Input, Select, DatePicker, Tooltip, Card} from 'antd';
+const { TextArea } = Input;
 import moment from 'moment';
-// import { DownOutlined, SearchOutlined } from '@ant-design/icons';
-
+import { InfoCircleOutlined } from '@ant-design/icons';
+import addAction from '@/assets/resource-action-add1.svg';
+import historyIcon from '@/assets/resource-management-edit1.svg';
+import datePickerIcon from '@/assets/resource-management-datepicker.svg'
 import empty from '@/assets/timeOffTableEmptyIcon.svg';
-import addIcon from '@/assets/resource-action-add.svg';
-import historyIcon from '@/assets/resource-management-edit.svg';
-
 import styles from './index.less';
 
 class TableTickets extends PureComponent {
@@ -20,6 +17,9 @@ class TableTickets extends PureComponent {
       // pageNavigation: '1',
       currentTime: moment(),
     };
+    // this.showModal= this.showModal.bind(this);
+    // this.handleOk = this.handleOk.bind(this);
+    // this.handleCancel = this.handleCancel.bind(this);
   }
 
   componentDidMount = () => {
@@ -37,10 +37,131 @@ class TableTickets extends PureComponent {
 
     if (check) {
       this.setState({
-        currentTime: moment(),
+        currentTime: moment()
       });
     }
   };
+  actionAddAndEdit = (row) => {
+    const [visible, setVisible] = useState(false);
+    function showModal () {
+      setVisible(true);
+    }
+
+    function handleOk (event) {
+      setVisible(false);
+    }
+
+    function handleCancel () {
+      setVisible(false);
+    }
+    return <div>
+    <img src={addAction} alt="attachIcon" onClick={showModal} className={styles.buttonAdd}/>       
+    <Modal
+    title="Assign to project"
+    width="60%"
+    visible={visible}
+    onOk={handleOk}
+    onCancel={handleCancel}
+    okText='Assign to project'
+    cancelButtonProps={{ style: { color: 'red', border: '1px solid white' } }}
+    okButtonProps={{style: {background: '#FFA100', border: '1px solid #FFA100', color: 'white', borderRadius: '25px'}}}
+  >
+    <Form layout='vertical'>
+    <Row>
+      <Col span={12}>
+        <Form.Item label='Project' name="project">  
+          <Select defaultValue={row.projectName} style={{ width: '95%', borderRadius: '8px' }}>
+            <Option value="jack">project 0</Option>
+            <Option value="lucy">project 1</Option>
+            <Option value="Yiminghe">project 2</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label='Status'>
+          <Select defaultValue={row.projectName} style={{ width: '95%', borderRadius: '8px' }}>
+            <Option value="jack">project 0</Option>
+            <Option value="lucy">project 1</Option>
+            <Option value="Yiminghe">project 2</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item label='Bandwith Allocation (%)'>
+        <Input placeholder="100" style={{width: '95%'}}
+          suffix={
+            <Tooltip title="Extra information">
+              <InfoCircleOutlined style={{ color: 'rgba(0,0,0,.45)' }} />
+            </Tooltip>
+          }
+        />
+        </Form.Item>
+      </Col>
+      <Col span={12}>
+        <Form.Item label='Start Date'>
+          <DatePicker placeholder='Enter Start Date' picker="week" 
+          style={{width: '95%', borderRadius: '8px', color: 'blue'}}
+          suffixIcon={<img src={datePickerIcon}/>}/>
+        </Form.Item>
+        <Form.Item label='End Date*'>
+          <DatePicker placeholder='Enter End Date' picker="week" 
+          style={{width: '95%', borderRadius: '8px', color: 'blue'}}
+          suffixIcon={<img src={datePickerIcon}/>}/>
+        </Form.Item>
+      </Col>
+    </Row>    
+    <Form.Item label='Comments (optional)'>
+      <TextArea placeholder="Enter Comments"
+        autoSize={{ minRows: 4, maxRows: 8 }}
+      />
+    </Form.Item>  
+    <p style={{color: 'lightgray'}}>*Tentative End Date</p>
+    <Form.Item label='Project Detail'>
+    <Card style={{background: '#F6F7F9'}}>
+      <Row>
+        <Col span={12}>
+          <p>Customer: <span style={{color: '#2C6DF9'}}> {row.employeeName}</span></p> 
+          <p>Project: <span style={{color: '#2C6DF9'}}> {row.projectName}</span></p> 
+          <p>Engagement Type: <span style={{color: '#2C6DF9'}}> {row.billStatus}</span></p> 
+          <p>Start Date: <span style={{color: '#2C6DF9'}}> {row.startDate}</span></p> 
+          <p>End Date: <span style={{color: '#2C6DF9'}}> {row.endDate}</span></p>  
+        </Col>
+        <Col span={12}>
+          <Row>
+            <Col span={12}>
+              Current resource allocation :
+            </Col>
+            <Col span={12}>
+              <p>2/3 UX Designers (Billable)</p>
+              <p>1/2 UI Designer (Billable)</p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Card>
+    </Form.Item>
+    </Form>
+  </Modal> 
+  <img src={historyIcon} alt="historyIcon" className={styles.buttonEdit}/>
+  </div>
+  }
+  //state = { visible: false }
+  // showModal = () => {
+  //   this.setState({
+  //     visible: true,
+  //   });
+  // }
+  // handleOk = (e) => {
+  //   console.log(e);
+  //   this.setState({
+  //     visible: false,
+  //   });
+  // }
+  // handleCancel = (e) => {
+  //   console.log(e);
+  //   this.setState({
+  //     visible: false,
+  //   });
+  // }
+  // showAlert = (row) => {
+  //   alert(JSON.stringify(row))
+  // }
 
   openViewTicket = (ticketID) => {
     const { data = [] } = this.props;
@@ -60,11 +181,9 @@ class TableTickets extends PureComponent {
   handleSelect = (e) => {
     e.preventDefault();
   };
-
   onTableChange = (pagination, filters, sorter, extra) => {
     console.log('params', pagination, filters, sorter, extra);
   };
-
   render() {
     const {
       data = [],
@@ -304,19 +423,7 @@ class TableTickets extends PureComponent {
         // dataIndex: 'subject',
         key: 'action',
         render: (value, row, col) => {
-          const buttonGroup = (
-            <Button.Group>
-              <Button
-                style={{ marginRight: '15px', width: '45px', height: '45px', borderRadius: '50%' }}
-                onClick={() => showAlert(value)}
-              >
-                <img src={addIcon} alt="attachIcon" />
-              </Button>
-              <Button style={{ width: '45px', height: '45px', borderRadius: '50%' }}>
-                <img src={historyIcon} alt="historyIcon" />
-              </Button>
-            </Button.Group>
-          );
+          const buttonGroup = this.actionAddAndEdit(row);
           const obj = renderCell('add', row, buttonGroup);
           if (col === size - 1) {
             mapping.clear();
