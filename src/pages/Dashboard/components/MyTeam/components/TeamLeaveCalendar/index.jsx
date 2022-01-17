42bfbf344e29b08b67694f021ee9c2ce8158ceb1
Lewis Nguyen <lewis.nguyen@terralogic.com>
2022-01-17 08:44:08 +0700

    fix bug

--- src/pages/Dashboard/components/MyTeam/components/TeamLeaveCalendar/index.jsx
+++ src/pages/Dashboard/components/MyTeam/components/TeamLeaveCalendar/index.jsx
@@ -1,5 +1,5 @@
 import { Avatar, Calendar, Tooltip } from 'antd';
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'umi';
 // import MockAvatar from '@/assets/dashboard/mockAvatar.jpg';
 import moment from 'moment';
@@ -7,7 +7,7 @@ import moment from 'moment';
 // import SampleAvatar2 from '@/assets/dashboard/sampleAvatar2.png';
 // import SampleAvatar3 from '@/assets/dashboard/sampleAvatar3.png';
 // import SampleAvatar4 from '@/assets/dashboard/sampleAvatar4.png';
-import mockAvatar from '@/assets/timeSheet/mockAvatar.jpg'
+import mockAvatar from '@/assets/timeSheet/mockAvatar.jpg';
 import styles from './index.less';
 
 // const leaveRequest = [
@@ -71,7 +71,7 @@ import styles from './index.less';
 const TeamLeaveCalendar = (props) => {
   const { selectedMonth = '', teamLeaveRequestList = [], dispatch, listTimeOffType = [] } = props;
   const startOfMonth = moment(selectedMonth).startOf('month').format('MM/DD/YYYY');
-  const endOfMonth   = moment(selectedMonth).endOf('month').format('MM/DD/YYYY');
+  const endOfMonth = moment(selectedMonth).endOf('month').format('MM/DD/YYYY');
   useEffect(() => {
     // refresh data by month here
     dispatch({
@@ -83,31 +83,44 @@ const TeamLeaveCalendar = (props) => {
         type: listTimeOffType,
         page: 1,
         limit: 100,
-        search: ""
+        search: '',
       },
-    })
+    });
   }, [selectedMonth]);
 
   // FUNCTIONS
   const checkDate = (date, listDate) => {
-    return listDate.filter((val) => moment(val.date).format('MM/DD/YYYY') === moment(date).format('MM/DD/YYYY')) || []
-  }
+    return (
+      listDate.filter(
+        (val) => moment(val.date).format('MM/DD/YYYY') === moment(date).format('MM/DD/YYYY'),
+      ) || []
+    );
+  };
   const getLeaveRequestOfDate = (date) => {
     // return leaveRequest.find((l) => l.date === moment(date).format('MM/DD/YYYY'));
     const filterDateLeave = teamLeaveRequestList.map((obj) => {
       return {
         employeeInfo: obj.employee,
-        duration: checkDate(date, obj.leaveDates)
-      }
-    })
+        duration: checkDate(date, obj.leaveDates),
+      };
+    });
     // return filterDateLeave.filter((obj) => obj.duration.timeOfDay !== undefined)
-    return filterDateLeave.filter((obj) => obj.duration.length > 0)
+    return filterDateLeave.filter((obj) => obj.duration.length > 0);
   };
 
-  // const getDateDuration = (duration) => {
-  //   if (duration === 8) return 'full day';
-  //   return `${duration}hrs`;
-  // };
+  const getDateDuration = (member) => {
+    const duration = member?.duration[0]?.timeOfDay;
+    switch (duration) {
+      case 'WHOLE-DAY':
+        return 'full day';
+      case 'MORNING':
+      case 'AFTERNOON':
+        return `4hrs`;
+
+      default:
+        return '';
+    }
+  };
 
   // to prevent clicking on date of prev/next month to change month
   const disabledDate = (currentDate) => {
@@ -121,7 +134,8 @@ const TeamLeaveCalendar = (props) => {
         {list.map((member) => (
           <span style={{ display: 'block' }}>
             {/* {member.name} ({getDateDuration(member.duration)}) */}
-            {member.employeeInfo.generalInfo.firstName} {member.employeeInfo.generalInfo.lastName} ({member ? member.duration[0].timeOfDay : ''})
+            {member.employeeInfo.generalInfo.firstName} {member.employeeInfo.generalInfo.lastName}{' '}
+            {member ? `(${getDateDuration(member)})` : ''}
           </span>
         ))}
         {/* <span style={{ display: 'block' }}>
@@ -145,7 +159,9 @@ const TeamLeaveCalendar = (props) => {
         <div className={styles.projectMembers}>
           <Avatar.Group maxCount={4}>
             {find.map((member) => {
-              return <Avatar size="large" src={member.employeeInfo.generalInfo.avatar || mockAvatar} />;
+              return (
+                <Avatar size="large" src={member.employeeInfo.generalInfo.avatar || mockAvatar} />
+              );
             })}
             {/* <Avatar size='large' src={find.employeeInfo.generalInfo.avatar} /> */}
           </Avatar.Group>
@@ -164,4 +180,6 @@ const TeamLeaveCalendar = (props) => {
   );
 };
 
-export default connect(({dashboard: { teamLeaveRequestList = [] } = {}}) => ({teamLeaveRequestList}))(TeamLeaveCalendar);
+export default connect(({ dashboard: { teamLeaveRequestList = [] } = {} }) => ({
+  teamLeaveRequestList,
+}))(TeamLeaveCalendar);
