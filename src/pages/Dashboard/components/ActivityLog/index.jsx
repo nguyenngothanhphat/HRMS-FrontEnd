37833e8a09c5d3a379aa4631b345b6f80fa827b6
Lam Tron <xeptron@gmail.com>
2022-01-25 15:53:32 +0700

    Truncate text

--- src/pages/Dashboard/components/ActivityLog/index.jsx
+++ src/pages/Dashboard/components/ActivityLog/index.jsx
@@ -164,6 +164,13 @@ const ActivityLog = (props) => {
         {renderShowAll()}
         <img src={LeftArrow} alt="expand" />
       </div>
+      {/* 
+      {activeKey === '1' && googleCalendarList.length > 0 && (
+        <div className={styles.viewAllMeetingBtn} onClick={() => setModalVisible(true)}>
+          <span>View all Meetings</span>
+          <img src={LeftArrow} alt="expand" />
+        </div>
+      )} */}
       <CommonModal
         visible={modalVisible}
         title={renderTabName(activeKey)}
