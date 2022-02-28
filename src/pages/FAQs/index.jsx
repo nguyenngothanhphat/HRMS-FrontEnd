c182d11ad06dbf6d6495c4c9012e2af77e1cda83
nghia.vo <nghia.vo@terralogic.com>
2022-02-28 15:45:42 +0700

    update code

--- src/pages/FAQs/index.jsx
+++ src/pages/FAQs/index.jsx
@@ -1,9 +1,4 @@
 import React, { PureComponent } from 'react';
-<<<<<<< HEAD
-=======
-// import { Affix, Row, Col, Button, Link } from 'antd';
-// import { formatMessage, connect } from 'umi';
->>>>>>> ef07394b06483abe097d99b097e0215c1d1bf279
 import { Row, Col, Button } from 'antd';
 import { connect, Link } from 'umi';
 import { PageContainer } from '@/layouts/layout/src';
@@ -12,7 +7,6 @@ import { PageContainer } from '@/layouts/layout/src';
 import FAQList from './components/FAQList';
 import styles from './index.less';
 
-const HR_MANAGER = 'HR-MANAGER';
 @connect(
   ({
     user: { currentUser = {}, permissions = {}, } = {},
@@ -37,7 +31,6 @@ class FAQs extends PureComponent {
   }
 
   render() {
-<<<<<<< HEAD
     const {
       permissions
     } = this.props;
@@ -58,47 +51,6 @@ class FAQs extends PureComponent {
                 ) : null}
               </div>
             </div>
-=======
-    // const { location: { query = {} } = {}, getListByCompany = {}, currentUser: { roles = [] } } = this.props;
-    const {
-      currentUser: { roles = [] },
-    } = this.props;
-    // const { faq = [] } = getListByCompany;
-    const checkRoleHrAndManager = roles.includes(HR_MANAGER);
-    return (
-      <PageContainer>
-        {/* <div className={styles.root}>
-          <Affix offsetTop={42}>
-            <div className={styles.titlePage}>
-              <div className={styles.titlePage__text}>
-                {formatMessage({ id: 'pages.frequentlyAskedQuestions.title' })}
-              </div>
-            </div>
-          </Affix>
-          <Row>
-            <Col span={17}>
-              <ListQuestions list={faq} idQuestion={query} />
-            </Col>
-            <Col span={7} className={styles.contactPage}>
-              <ContactPage />
-            </Col>
-          </Row>
-        </div> */}
-        <Row className={styles.FAQs}>
-          <Col span={24}>
-            <div className={styles.header}>
-              <div className={styles.header__left}>FAQs</div>
-              <div className={styles.header__right}>
-                {checkRoleHrAndManager ? (
-                  <Button>
-                    <Link to="/faqpage/settings">
-                      <span className={styles.buttonSetting__text}>Settings</span>
-                    </Link>
-                  </Button>
-                ) : null}
-              </div>
-            </div>
->>>>>>> ef07394b06483abe097d99b097e0215c1d1bf279
           </Col>
           <Col span={24} />
           <Col span={24}>
