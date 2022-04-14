8b0facafe68c58833dd867ae8b243ebdaacb76eb
Sinh Tran <sinh.tran@terralogic.com>
2022-04-14 11:26:46 +0700

    update UI

--- src/pages/TimeOff/components/SetupTimeoff/components/TimeOffType/components/TypeConfiguration/components/AnnualResetPolicy/index.jsx
+++ src/pages/TimeOff/components/SetupTimeoff/components/TimeOffType/components/TypeConfiguration/components/AnnualResetPolicy/index.jsx
@@ -1,6 +1,7 @@
 import { Card, Col, Form, DatePicker, Row, Radio, Space } from 'antd';
 import React, { useState } from 'react';
 import { connect } from 'umi';
+import datePickerIcon from '@/assets/timeOff/datePicker.svg';
 import styles from './index.less';
 
 const AnnualResetPolicy = () => {
@@ -8,34 +9,33 @@ const AnnualResetPolicy = () => {
   const onChange = () => {};
   return (
     <Card title="Negative Leave Balance" className={styles.AnnualResetPolicy}>
-      <Row gutter={[24, 24]} align="middle">
-        <Col sm={18}>
-          <div className={styles.annualReset}>
-            <span className={styles.label}>How do you want to reset?</span>
-            <Radio.Group name="accrualMethod" defaultValue={1}>
-              <Space direction="horizontal">
-                <Radio value={1}>Anniversary Date</Radio>
-                <Radio value={2}>Calendar Date</Radio>
-              </Space>
-            </Radio.Group>
-          </div>
-        </Col>
-      </Row>
+      <div className={styles.annualResetTop}>
+        <span className={styles.label}>How do you want to reset?</span>
+        <Radio.Group name="accrualMethod" defaultValue={1}>
+          <Space direction="horizontal">
+            <Radio value={1}>Anniversary Date</Radio>
+            <Radio value={2}>Calendar Date</Radio>
+          </Space>
+        </Radio.Group>
+      </div>
 
-      <Row gutter={[24, 24]} align="middle">
-        <span className={styles.label}>Select Date</span>
-        <Col sm={18}>
+      <Row>
+        <Col sm={5}>
+          <span className={styles.label}>Select Date</span>
+        </Col>
+        <Col sm={8}>
           <Form.Item>
             <DatePicker
+              suffixIcon={<img src={datePickerIcon} alt="" />}
               onChange={onChange}
-              format={isOpen ? 'MM-DD-YYYY' : 'dddd'}
+              format={isOpen ? 'MM-DD-YYYY' : 'dddd Do'}
               onOpenChange={(status) => {
                 setIsOpen(status);
               }}
             />
           </Form.Item>
         </Col>
-        <Col sm={14} />
+        <Col sm={11} />
       </Row>
     </Card>
   );
