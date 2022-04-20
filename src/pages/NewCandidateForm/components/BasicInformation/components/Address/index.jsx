0ec6d3b99f6d584b4fe07bfd9b065c5484954185
Lewis Nguyen <lewis.nguyen@terralogic.com>
2022-04-20 16:11:17 +0700

    add more fields for basic info page - onboarding HR flow

---
+++ src/pages/NewCandidateForm/components/BasicInformation/components/Address/index.jsx
@@ -0,0 +1,158 @@
+import { Checkbox, Col, Form, Input, Row } from 'antd';
+import React from 'react';
+import { connect } from 'umi';
+import styles from './index.less';
+
+const Address = (props) => {
+  const {
+    disabled = false,
+    onSameAddress = () => {},
+    // isSameAddress = false,
+    // form,
+  } = props;
+
+  // useEffect(() => {
+  //   ['AddressLine1', 'AddressLine2', 'Country', 'City', 'State', 'ZipCode'].forEach((x) => {
+  //     const input = form.getFieldInstance(`permanent${x}`);
+  //     if (input) {
+  //       input.input.disabled = isSameAddress;
+  //     }
+  //   });
+  // }, [isSameAddress]);
+
+  const fields = [
+    {
+      name: 'AddressLine1',
+      label: 'Address Line 1',
+      rules: [
+        {
+          required: true,
+          message: 'Required field',
+        },
+      ],
+      span: {
+        xs: 24,
+        md: 24,
+      },
+      component: <Input disabled={disabled} autoComplete="off" placeholder="Address Line 1" />,
+    },
+    {
+      name: 'AddressLine2',
+      label: 'Address Line 2',
+      span: {
+        xs: 24,
+        md: 24,
+      },
+      component: <Input disabled={disabled} autoComplete="off" placeholder="Address Line 2" />,
+    },
+    {
+      name: 'Country',
+      label: 'Country',
+      rules: [
+        {
+          required: true,
+          message: 'Required field',
+        },
+      ],
+      span: {
+        xs: 24,
+        md: 12,
+      },
+      component: <Input disabled={disabled} autoComplete="off" placeholder="Country" />,
+    },
+    {
+      name: 'State',
+      label: 'State',
+      rules: [
+        {
+          required: true,
+          message: 'Required field',
+        },
+      ],
+      span: {
+        xs: 24,
+        md: 12,
+      },
+      component: <Input disabled={disabled} autoComplete="off" placeholder="State" />,
+    },
+    {
+      name: 'City',
+      label: 'City',
+      rules: [
+        {
+          required: true,
+          message: 'Required field',
+        },
+      ],
+      span: {
+        xs: 24,
+        md: 12,
+      },
+      component: <Input disabled={disabled} autoComplete="off" placeholder="City" />,
+    },
+    {
+      name: 'ZipCode',
+      label: 'Zip/Postal Code',
+      rules: [
+        {
+          required: true,
+          message: 'Required field',
+        },
+      ],
+      span: {
+        xs: 24,
+        md: 12,
+      },
+      component: <Input disabled={disabled} autoComplete="off" placeholder="Zip Code" />,
+    },
+  ];
+
+  return (
+    <div className={styles.Address}>
+      <div className={styles.form}>
+        <Row gutter={[24, 0]}>
+          {fields.map((x) => (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <Col {...x.span}>
+              <Form.Item
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }}
+                label={x.label}
+                name={`current${x.name}`}
+                rules={x.rules}
+              >
+                {x.component}
+              </Form.Item>
+            </Col>
+          ))}
+        </Row>
+      </div>
+      <div className={styles.addressTitle}>
+        Permanent Address
+        <Checkbox onChange={onSameAddress} disabled={disabled}>
+          Same as above
+        </Checkbox>
+      </div>
+      <div className={styles.form}>
+        <Row gutter={[24, 0]}>
+          {fields.map((x) => (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <Col {...x.span}>
+              <Form.Item
+                labelCol={{ span: 24 }}
+                wrapperCol={{ span: 24 }}
+                label={x.label}
+                name={`permanent${x.name}`}
+                rules={x.rules}
+              >
+                {x.component}
+              </Form.Item>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </div>
+  );
+};
+
+export default connect(() => ({}))(Address);
---
+++ src/pages/NewCandidateForm/components/BasicInformation/components/BasicInfo/index.jsx
@@ -0,0 +1,194 @@
+import { Col, Form, Input, Row } from 'antd';
+import React from 'react';
+import { connect, formatMessage } from 'umi';
+import styles from './index.less';
+
+const BasicInfo = (props) => {
+  const { disabled = false, testValidate = () => {} } = props;
+
+  return (
+    <div className={styles.BasicInfo}>
+      <Row gutter={[24, 0]}>
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            label={formatMessage({ id: 'component.basicInformation.firstName' })}
+            name="firstName"
+            rules={[
+              { required: true, message: 'Required field' },
+              {
+                pattern: /[^\s-]/,
+                message: 'First name is invalid!',
+              },
+            ]}
+          >
+            <Input
+              disabled={disabled}
+              autoComplete="off"
+              // onChange={(e) => handleChange(e)}
+              placeholder="First Name"
+              className={styles.formInput}
+            />
+          </Form.Item>
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            label={formatMessage({ id: 'component.basicInformation.middleName' })}
+            name="middleName"
+            rules={[
+              {
+                pattern: /[^\s-]/,
+                message: 'Middle name is invalid!',
+              },
+            ]}
+          >
+            <Input
+              disabled={disabled}
+              autoComplete="off"
+              // onChange={(e) => handleChange(e)}
+              placeholder="Middle Name"
+              className={styles.formInput}
+            />
+          </Form.Item>
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            label={formatMessage({ id: 'component.basicInformation.lastName' })}
+            name="lastName"
+            rules={[
+              { required: true, message: 'Required field' },
+              {
+                pattern: /[^\s-]/,
+                message: 'Last name is invalid!',
+              },
+            ]}
+          >
+            <Input
+              disabled={disabled}
+              autoComplete="off"
+              // onChange={(e) => handleChange(e)}
+              placeholder="Last Name"
+              className={styles.formInput}
+            />
+          </Form.Item>
+        </Col>
+
+        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            label="Total Experience in years"
+            name="totalExperience"
+            rules={[
+              {
+                pattern: /\b([0-9]|[1-9][0-9])\b/,
+                message: 'Invalid format',
+              },
+              {
+                pattern: /^\d+$/,
+                message: 'Only digit!',
+              },
+            ]}
+          >
+            <Input
+              disabled={disabled}
+              autoComplete="off"
+              placeholder="Total Experience in years"
+              className={styles.formInput}
+            />
+          </Form.Item>
+        </Col>
+
+        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            label="Relevant Experience in years"
+            name="previousExperience"
+            rules={[
+              {
+                pattern: /\b([0-9]|[1-9][0-9])\b/,
+                message: 'Invalid format',
+              },
+              {
+                pattern: /^\d+$/,
+                message: 'Only digit!',
+              },
+            ]}
+          >
+            <Input
+              disabled={disabled}
+              autoComplete="off"
+              placeholder="Relevant Experience in years"
+              className={styles.formInput}
+            />
+          </Form.Item>
+        </Col>
+
+        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            label={formatMessage({ id: 'component.basicInformation.privateEmail' })}
+            name="privateEmail"
+            rules={[
+              { required: true, message: 'Required field' },
+              {
+                type: 'email',
+                message: 'Invalid email',
+              },
+              ({ getFieldValue }) => ({
+                validator(rule, value) {
+                  if (!value || getFieldValue('workEmail') !== value) {
+                    return Promise.resolve();
+                  }
+                  // eslint-disable-next-line compat/compat
+                  return Promise.reject(new Error('Two emails cannot be the same!'));
+                },
+              }),
+            ]}
+          >
+            <Input
+              disabled={disabled}
+              autoComplete="off"
+              // onChange={(e) => handleChange(e)}
+              placeholder="Personal Email"
+              className={styles.formInput}
+              onChange={testValidate}
+            />
+          </Form.Item>
+        </Col>
+
+        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+          <Form.Item
+            labelCol={{ span: 24 }}
+            wrapperCol={{ span: 24 }}
+            label="Phone Number"
+            name="phoneNumber"
+            rules={[
+              { required: true, message: 'Required field' },
+              {
+                pattern: /^\d+$/,
+                message: 'Only digit !',
+              },
+            ]}
+          >
+            <Input
+              disabled={disabled}
+              autoComplete="off"
+              placeholder="Phone Number"
+              className={styles.formInput}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default connect(() => ({}))(BasicInfo);
