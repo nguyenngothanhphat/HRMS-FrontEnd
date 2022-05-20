272d99a06e16bc9b93d1bf4b8f66ed1a38a44a26
Lewis Nguyen <lewis.nguyen@terralogic.com>
2022-05-20 11:08:23 +0700

    change project font, fix timeoff tables

--- src/pages/TimeOff/components/Overview/components/LeaveInformation/components/LeaveProgressBar/index.jsx
+++ src/pages/TimeOff/components/Overview/components/LeaveInformation/components/LeaveProgressBar/index.jsx
@@ -4,52 +4,43 @@ import styles from './index.less';
 import { addZeroToNumber, roundNumber } from '@/utils/timeOff';
 
 export default class LeaveProgressBar extends PureComponent {
-  renderCircle = (stepNumber, limitNumber, color) => {
+  renderCircle = (color) => {
+    const { type: { remainingTotalMessage = '', taken = 0, total = 0 } = {} } = this.props;
     return (
       <span
         className={styles.smallCircle}
         style={{
-          fontSize: stepNumber % 1 !== 0 || limitNumber % 1 !== 0 ? 10 : 12,
+          fontSize: (total - taken) % 1 !== 0 || total % 1 !== 0 ? 10 : 12,
         }}
       >
-        <span style={{ color, fontWeight: 'bold' }}>{roundNumber(stepNumber)}</span>/
-        {addZeroToNumber(limitNumber)}
+        <span style={{ color, fontWeight: 'bold' }}>{remainingTotalMessage}</span>
       </span>
     );
   };
 
-  renderProgressBar = () => {
-    const { color = '', stepNumber = 0, limitNumber = 0 } = this.props;
-    const remaining = (stepNumber / limitNumber) * 100;
-    return (
-      <div className={styles.renderProgressBar}>
-        <Progress
-          type="circle"
-          percent={remaining}
-          width={52}
-          strokeColor={color}
-          trailColor="#d6dce0"
-          format={() => this.renderCircle(stepNumber, limitNumber, color)}
-        />
-      </div>
-    );
-  };
-
   render() {
-    const { title = '', shortType = '', stepNumber = 0 } = this.props;
+    const { type: { remainingMessage = '', taken = 0, total = 0, name = '' } = {}, color = '' } =
+      this.props;
     return (
       <div className={styles.LeaveProgressBar}>
         <div className={styles.LeaveProgressBar__above}>
-          <span className={styles.title}>
-            {title}
-            <span className={styles.title__shorten}> {shortType && `(${shortType})`}</span>
-          </span>
+          <span className={styles.title}>{name}</span>
           <span className={styles.progress}>
-            <span className={styles.stepNumber}>Remaining: {roundNumber(stepNumber)}</span>
-            {/* <span className={styles.limitNumber}>/{`0${limitNumber}`.slice(-2)}</span> */}
+            <span className={styles.stepNumber}>Remaining: {remainingMessage}</span>
           </span>
         </div>
-        <div className={styles.LeaveProgressBar__below}>{this.renderProgressBar()}</div>
+        <div className={styles.LeaveProgressBar__below}>
+          <div className={styles.renderProgressBar}>
+            <Progress
+              type="circle"
+              percent={((total - taken) * 100) / total}
+              width={52}
+              strokeColor={color}
+              trailColor="#d6dce0"
+              format={() => this.renderCircle(color)}
+            />
+          </div>
+        </div>
       </div>
     );
   }
