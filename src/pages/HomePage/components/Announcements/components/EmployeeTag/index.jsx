7e56c6c7a749b61d179c7ef24ffaf3637f10465b
Min-Tri <56967379+Min-Tri@users.noreply.github.com>
2022-06-03 11:22:21 +0700

    add a timestamp to the announcements on the home page

--- src/pages/HomePage/components/Announcements/components/EmployeeTag/index.jsx
+++ src/pages/HomePage/components/Announcements/components/EmployeeTag/index.jsx
@@ -1,16 +1,25 @@
 import { Col } from 'antd';
 import React from 'react';
 import { connect, history } from 'umi';
+import moment from 'moment';
 import MockAvatar from '@/assets/dashboard/mockAvatar.jpg';
 import styles from './index.less';
+import { getTimezoneViaCity } from '@/utils/times';
 
 const EmployeeTag = (props) => {
   const {
     employee: {
       generalInfoInfo: { avatar = '', legalName = '', userId = '', website = '' } = {} || {},
+      location: { state = '', countryName = '' } = {},
+      locationInfo,
       titleInfo = {} || {},
     } = {} || {},
+    createDate,
   } = props;
+
+  const {
+    headQuarterAddress: { state: state1 = '', country: { name: countryName1 = '' } = {} } = {},
+  } = locationInfo || {};
 
   const onViewProfileClick = () => {
     if (website) {
@@ -20,8 +29,17 @@ const EmployeeTag = (props) => {
     }
   };
 
+  const Timestamp = () => {
+    const getTimezone =
+      getTimezoneViaCity(state || state1) || getTimezoneViaCity(countryName || countryName1) || '';
+    const timezone =
+      getTimezone !== '' ? getTimezone : Intl.DateTimeFormat().resolvedOptions().timeZone;
+    const date = moment(createDate).tz(timezone).locale('en').format('MMMM/DD/YYYY, HH:mm A');
+    return <span className={styles.timestamp}>{date}</span>;
+  };
+
   return (
-    <Col span={24} className={styles.EmployeeTag} onClick={onViewProfileClick}>
+    <Col className={styles.EmployeeTag} span={12} onClick={onViewProfileClick}>
       <div className={styles.container}>
         <div className={styles.avatar}>
           <img src={avatar || MockAvatar} alt="" />
@@ -29,6 +47,7 @@ const EmployeeTag = (props) => {
         <div className={styles.information}>
           <span className={styles.name}>{legalName}</span>
           <span className={styles.title}>{titleInfo?.name}</span>
+          <Timestamp />
         </div>
       </div>
     </Col>
