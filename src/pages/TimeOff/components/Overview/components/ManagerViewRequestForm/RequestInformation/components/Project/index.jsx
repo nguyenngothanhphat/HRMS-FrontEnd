fe6eef1418c0b697337daa2068601d8b31d1995e
Sinh Tran <sinh.tran@terralogic.com>
2022-07-06 15:43:46 +0700

    integrate project with timeoff

--- src/pages/TimeOff/components/Overview/components/ManagerViewRequestForm/RequestInformation/components/Project/index.jsx
+++ src/pages/TimeOff/components/Overview/components/ManagerViewRequestForm/RequestInformation/components/Project/index.jsx
@@ -1,40 +1,98 @@
 import React, { Component } from 'react';
-import { Row, Col, Progress } from 'antd';
-import { history } from 'umi';
+import { Row, Col } from 'antd';
+import { history, Link } from 'umi';
+import moment from 'moment';
 import styles from './index.less';
+import { TIMEOFF_DATE_FORMAT } from '@/utils/timeOff';
+import UserProfilePopover from '@/components/UserProfilePopover';
+import ProjectDetailPopover from '@/components/ProjectDetailPopover';
 
 class Project extends Component {
-  // ON VIEW EMPLOYEE PROFILE
-  onViewEmployeeProfile = (_id) => {
-    history.push({
-      pathname: `/directory/employee-profile/${_id}`,
-    });
+  onViewProfileClick = (userId) => {
+    if (userId) {
+      history.push(`/directory/employee-profile/${userId}/general-info`);
+    }
+  };
+
+  dataHover = (data) => {
+    const {
+      generalInfo: {
+        legalName = '',
+        avatar: avatar1 = '',
+        userId = '',
+        workEmail = '',
+        workNumber = '',
+        skills = [],
+      } = {},
+      generalInfo = {},
+      department = {},
+      location: locationInfo = {},
+      manager: managerInfo = {},
+      title = {},
+    } = data;
+    return {
+      legalName,
+      userId,
+      department,
+      workEmail,
+      workNumber,
+      locationInfo,
+      generalInfo,
+      managerInfo,
+      title,
+      avatar1,
+      skills,
+    };
+  };
+
+  viewProject = (projectId) => {
+    return `/project-management/list/${projectId}/summary`;
   };
 
   render() {
-    const { name = '', projectManager = '', projectHealth = 0, employeeId = '' } = this.props;
+    const {
+      project: { projectName, id: projectId = '' } = {},
+      projectManager: { generalInfo: { legalName: managerName = '', userId = '' } = {} } = {},
+      projectManager = {},
+      infomationProject = {},
+      startDate = '',
+      endDate = '',
+    } = this.props;
     return (
       <div className={styles.Project}>
         <Row>
-          <Col span={6} className={styles.detailColumn}>
-            <span>{name}</span>
-          </Col>
-          <Col span={6} className={styles.detailColumn}>
-            <span
-              onClick={() => this.onViewEmployeeProfile(employeeId)}
-              className={styles.employeeLink}
+          <Col span={5} className={styles.detailColumn}>
+            <ProjectDetailPopover
+              data={{ ...infomationProject }}
+              trigger="hover"
+              placement="topRight"
             >
-              {projectManager}
-            </span>
+              <Link className={styles.projectName} to={this.viewProject(projectId)}>
+                {projectName || '-'}
+              </Link>
+            </ProjectDetailPopover>
           </Col>
-          <Col span={12} className={styles.detailColumn}>
+          <Col span={5} className={styles.detailColumn}>
+            <UserProfilePopover data={this.dataHover(projectManager)}>
+              <span className={styles.managerName} onClick={() => this.onViewProfileClick(userId)}>
+                {managerName}
+              </span>
+            </UserProfilePopover>
+          </Col>
+          <Col span={5} className={styles.detailColumn}>
+            <span>{moment(startDate).format(TIMEOFF_DATE_FORMAT) || '-'}</span>
+          </Col>
+          <Col span={5} className={styles.detailColumn}>
+            <span>{moment(endDate).format(TIMEOFF_DATE_FORMAT) || '-'}</span>
+          </Col>
+          <Col span={4} className={styles.detailColumn}>
             <div className={styles.projectHealth}>
-              <span className={styles.bar}>
+              {/* <span className={styles.bar}>
                 <Progress strokeLinecap="square" strokeColor="#00C598" percent={projectHealth} />
               </span>
               <span className={styles.viewReport} onClick={this.onViewReport}>
                 View Report
-              </span>
+              </span> */}
             </div>
           </Col>
         </Row>
