d4cba6f4a094eb5141f201e362940b462568014e
Viet Tran <viet.tran@infonam.com>
2022-07-15 15:42:12 +0700

    hrms-web-1769: fix ticket management location

--- src/utils/ticketManagement.js
+++ src/utils/ticketManagement.js
@@ -1,7 +1,14 @@
+import { debounce } from 'lodash';
+
 const LOCATION = {
   Headquarter: 'Headquarter',
   VN: 'VN',
   INDIA: 'INDIA',
   USA: 'USA',
 };
+
+export const debouncedChangeLocation = debounce((callback) => {
+  callback();
+}, 1000);
+
 export default { LOCATION };
