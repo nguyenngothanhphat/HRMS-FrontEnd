f2ecc11fd9e244b12e3add7fc55e07c38a8aadf9
tuandat8452 <dat.nguyen@terralogic.com>
2022-08-01 09:57:02 +0700

    hrms-web-80: Remove homepage for super admin

--- config/routes.js
+++ config/routes.js
@@ -229,6 +229,20 @@ const routes = [
             name: 'home',
             icon: '/assets/images/menuIcons/home.svg',
             component: './HomePage',
+            authority: [
+              HR_MANAGER,
+              HR,
+              EMPLOYEE,
+              REGION_HEAD,
+              CEO,
+              MANAGER,
+              ADMIN,
+              DEPARTMENT_HEAD,
+              PROJECT_MANAGER,
+              PEOPLE_MANAGER,
+              FINANCE,
+              CANDIDATE,
+            ],
           },
           {
             path: '/home/settings',
@@ -540,7 +554,7 @@ const routes = [
             authority: [
               'P_TIMEOFF_T_TEAM_REQUEST_HR_VIEW',
               'P_TIMEOFF_T_TEAM_REQUEST_MANAGER_VIEW',
-              OWNER
+              OWNER,
             ],
           },
 
@@ -703,14 +717,14 @@ const routes = [
             name: 'reviewResignationTicket',
             component: './OffBoarding/components/ManagerView/components/TicketDetails',
             hideInMenu: true,
-            authority: ['P_OFFBOARDING_VIEW','M_OFFBOARDING_VIEW'],
+            authority: ['P_OFFBOARDING_VIEW', 'M_OFFBOARDING_VIEW'],
           },
           {
             path: '/offboarding/list/hr-review/:id',
             name: 'reviewResignationTicket',
             component: './OffBoarding/components/HRView/components/TicketDetails',
             hideInMenu: true,
-            authority: ['P_OFFBOARDING_VIEW','M_OFFBOARDING_VIEW'],
+            authority: ['P_OFFBOARDING_VIEW', 'M_OFFBOARDING_VIEW'],
           },
           // {
           //   path: '/offboarding/my-request/:id',
@@ -755,24 +769,21 @@ const routes = [
             path: '/offboarding/settings/:type/form-detail/:id/view',
             name: 'offboarding.setting.form.viewForm',
             hideInMenu: true,
-            component:
-              './OffBoarding/components/Settings/components/Forms/components/ViewForm',
+            component: './OffBoarding/components/Settings/components/Forms/components/ViewForm',
             authority: ['P_OFFBOARDING_VIEW'],
           },
           {
             path: '/offboarding/settings/:type/form-detail/add',
             name: 'offboarding.setting.form.addForm',
             hideInMenu: true,
-            component:
-              './OffBoarding/components/Settings/components/Forms/components/HandleForm',
+            component: './OffBoarding/components/Settings/components/Forms/components/HandleForm',
             authority: ['P_OFFBOARDING_VIEW'],
           },
           {
             path: '/offboarding/settings/:type/form-detail/:id/edit',
             name: 'offboarding.setting.form.editForm',
             hideInMenu: true,
-            component:
-              './OffBoarding/components/Settings/components/Forms/components/HandleForm',
+            component: './OffBoarding/components/Settings/components/Forms/components/HandleForm',
             authority: ['P_OFFBOARDING_VIEW'],
           },
           {
--- src/components/Authorized/Authorized.jsx
+++ src/components/Authorized/Authorized.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
 import { Result } from 'antd';
+import React, { useEffect } from 'react';
+import { history } from 'umi';
 import check from './CheckPermissions';
 
-const Authorized = ({
-  children,
-  authority,
-  noMatch = (
-    <Result
-      status="403"
-      title="403"
-      subTitle="Sorry, you are not authorized to access this page."
-    />
-  ),
-}) => {
+const Authorized = (props) => {
+  const {
+    children,
+    authority,
+    noMatch = (
+      <Result
+        status="403"
+        title="403"
+        subTitle="Sorry, you are not authorized to access this page."
+      />
+    ),
+  } = props;
   const childrenRender = typeof children === 'undefined' ? null : children;
   const dom = check(authority, childrenRender, noMatch);
+  const signInRole = localStorage.getItem('antd-pro-authority');
+  const params = window.location.pathname;
+  useEffect(() => {
+    if (signInRole?.includes('owner') && params === '/home') {
+      history.push('/admin-app');
+    }
+  }, [JSON.stringify(signInRole)]);
   return <>{dom}</>;
 };
 
