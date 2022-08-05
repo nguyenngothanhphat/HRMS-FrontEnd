19d51328d3fc0e591019bba0c3d5b460d0591344
thang pham <thang.pham@terralogic.com>
2022-08-05 09:52:51 +0700

    update firebase config

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: 'AIzaSyCdFX1Q0RGLLkiB6TyxgYidfR_GPxWSGEk',
-  authDomain: 'stging-hrms.firebaseapp.com',
-  projectId: 'stging-hrms',
-  storageBucket: 'stging-hrms.appspot.com',
-  messagingSenderId: '216764339619',
-  appId: '1:216764339619:web:50ce5068c5075df970b616',
+  apiKey: 'AIzaSyBh_j3r1MmMAqgMO0Nf7Vy5ygm-cUH_4fQ',
+  authDomain: 'ambient-future-356403.firebaseapp.com',
+  projectId: 'ambient-future-356403',
+  storageBucket: 'ambient-future-356403.appspot.com',
+  messagingSenderId: '1010426690737',
+  appId: '1:1010426690737:web:0cd5e6dd332a4f66afb690',
 };
 
 export default firebaseConfig;
